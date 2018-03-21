dfc0ca9ac2ff07545327bcfc2c9ce87ccf103109
Stuart Slattery <slatterysr@ornl.gov>
2018-03-21 14:43:39 -0400

    adding cuda performance test

---
+++ core/example/cuda_perf_test.cpp
@@ -0,0 +1,143 @@
+#include <Cabana_AoSoA.hpp>
+#include <Cabana_Parallel.hpp>
+#include <Cabana_ExecutionPolicy.hpp>
+#include <Cabana_InnerArraySize.hpp>
+#include <Cabana_Index.hpp>
+#include <Cabana_MemberSlice.hpp>
+
+#include <Kokkos_Core.hpp>
+
+#include <iostream>
+#include <chrono>
+
+int main( int argc, char* argv[] )
+{
+    // Initialize the kokkos runtime.
+    Kokkos::initialize( argc, argv );
+
+    // Declare the inner array size.
+    using inner_array_size = Cabana::InnerArraySize<1000>;
+
+    // Declare data types.
+    using DataTypes =
+        Cabana::MemberDataTypes<double[3][3], // M1
+                                double[3][3], // M2
+                                double[3],    // V1
+                                double[3],    // V2
+                                double[3],    // RESULT
+                                double,       // S1
+                                double,       // S2
+                                int>;         // ID
+
+    // Enumerate the types for convenience.
+    enum MyTypes { M1 = 0,
+                   M2,
+                   V1,
+                   V2,
+                   RESULT,
+                   S1,
+                   S2,
+                   ID };
+
+    // Declare the AoSoA type. Use CudaUVM as the memory space.
+    using AoSoA_t = Cabana::AoSoA<DataTypes,inner_array_size,Kokkos::CudaUVMSpace>;
+
+    // Set the total problem size.
+    std::size_t num_data = 1e7;
+
+    // Create an Array-of-Structs-of-Arrays.
+    AoSoA_t aosoa( num_data );
+
+    // Make some slices.
+    auto m1 = Cabana::slice<M1>( aosoa );
+    auto m2 = Cabana::slice<M2>( aosoa );
+    auto v1 = Cabana::slice<V1>( aosoa );
+    auto v2 = Cabana::slice<V2>( aosoa );
+    auto result = Cabana::slice<RESULT>( aosoa );
+    auto s1 = Cabana::slice<S1>( aosoa );
+    auto s2 = Cabana::slice<S2>( aosoa );
+    auto id = Cabana::slice<ID>( aosoa );
+
+    // Create an execution policy. Execute with CUDA>
+    Cabana::IndexRangePolicy<Kokkos::Cuda>
+        range_policy( aosoa.begin(), aosoa.end() );
+
+    // Initialization functor.
+    auto init_func = KOKKOS_LAMBDA( const Cabana::Index idx )
+    {
+        for ( int i = 0; i < 3; ++i )
+            for ( int j = 0; j < 3; ++j )
+                m1(idx,i,j) = 1.0;
+
+        for ( int i = 0; i < 3; ++i )
+            for ( int j = 0; j < 3; ++j )
+                m2(idx,i,j) = 1.0;
+
+        for ( int i = 0; i < 3; ++i )
+            v1(idx,i) = 1.0;
+
+        for ( int i = 0; i < 3; ++i )
+            v2(idx,i) = 1.0;
+
+        s1(idx) = 1.0;
+        s2(idx) = 1.0;
+
+        id(idx) = 1.0;
+    };
+
+    // Initialize.
+    Cabana::parallel_for( range_policy, init_func, Cabana::StructAndArrayParallelTag() );
+
+    // Create a work functor:
+    // m3 = m1 * m2
+    // m1 * v1 / s1 + m2 * v2 / s2 + dot(v1,v2) * v1 + m3 * v2
+    auto work_func = KOKKOS_LAMBDA( const Cabana::Index idx )
+    {
+        double t1[3] = {0.0, 0.0, 0.0};
+        for ( int i = 0; i < 3; ++i )
+            for ( int j = 0; j < 3; ++j )
+                t1[i] += m1(idx,i,j) * v1(idx,j);
+
+        double t2[3] = {0.0, 0.0, 0.0};
+        for ( int i = 0; i < 3; ++i )
+            for ( int j = 0; j < 3; ++j )
+                t2[i] += m2(idx,i,j) * v2(idx,j);
+
+        double dotv1v2 = 0.0;
+        for ( int i = 0; i < 3; ++i )
+            dotv1v2 += v1(idx,i) * v2(idx,i);
+
+        double m3[3][3] = {{0.0, 0.0, 0.0},
+                           {0.0, 0.0, 0.0},
+                           {0.0, 0.0, 0.0}};
+        for ( int i = 0; i < 3; ++i )
+            for ( int j = 0; j < 3; ++j )
+                for ( int k = 0; k < 3; ++k )
+                    m3[i][j] += m1(idx,i,k) * m2(idx,k,j);
+
+        double t3[3] = {0.0, 0.0, 0.0};
+        for ( int i = 0; i < 3; ++i )
+            for ( int j = 0; j < 3; ++j )
+                t3[i] += m3[i][j] * v2(idx,j);
+
+        for ( int i = 0; i < 3; ++i )
+            result(idx,i) = t1[i] / s1(idx) +
+                            t2[i] / s2(idx) +
+                            dotv1v2 * v1(idx,i) +
+                            t3[i];
+    };
+
+    // Do work.
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Cabana::parallel_for( range_policy, work_func, Cabana::StructParallelTag() );
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    auto elapsed_time = end_time - start_time;
+    auto ms_elapsed =
+        std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_time);
+    std::cout << "Run time: " << ms_elapsed.count() << "ms" << std::endl;
+
+    // Finalize.
+    Kokkos::finalize();
+    return 0;
+}
