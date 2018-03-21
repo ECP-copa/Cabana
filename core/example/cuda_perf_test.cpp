c5cc6220289750655f9c42c95dbd84f2df527ec6
Stuart Slattery <slatterysr@ornl.gov>
2018-03-21 14:49:04 -0400

    changing tag in example

--- core/example/cuda_perf_test.cpp
+++ core/example/cuda_perf_test.cpp
@@ -129,7 +129,7 @@ int main( int argc, char* argv[] )
 
     // Do work.
     auto start_time = std::chrono::high_resolution_clock::now();
-    Cabana::parallel_for( range_policy, work_func, Cabana::StructParallelTag() );
+    Cabana::parallel_for( range_policy, work_func, Cabana::StructAndArrayParallelTag() );
     auto end_time = std::chrono::high_resolution_clock::now();
 
     auto elapsed_time = end_time - start_time;
