034006c2d012a1c3dc40613b3dd81bfca750f9cb
Stuart Slattery <slatterysr@ornl.gov>
2018-09-17 12:38:47 -0400

    adding BSD 3-clause license

--- core/unit_test/Cuda/tstDeepCopy_CudaUVM.cpp
+++ core/unit_test/Cuda/tstDeepCopy_CudaUVM.cpp
@@ -1,2 +1,13 @@
+/****************************************************************************
+ * Copyright (c) 2018 by the Cabana authors                                 *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the Cabana library. Cabana is distributed under a   *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
 #include <Cuda/TestCudaUVM_Category.hpp>
 #include <tstDeepCopy.hpp>
