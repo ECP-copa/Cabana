6b9b1f85c80aa54926246a8318b370bb06f78efd
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2018-10-11 15:27:17 +0000

    Documentation Update

--- doxygen/search/all_f.js
+++ doxygen/search/all_f.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['randomaccessmemory',['RandomAccessMemory',['../structCabana_1_1RandomAccessMemory.html',1,'Cabana']]],
-  ['rangebegin',['rangeBegin',['../classCabana_1_1LinkedCellList.html#a44a750305f79727a4982e729536d6558',1,'Cabana::LinkedCellList']]],
-  ['rangeend',['rangeEnd',['../classCabana_1_1LinkedCellList.html#a0d8529a197a5fcbc0b7b7a10b4f1b478',1,'Cabana::LinkedCellList']]],
+  ['rangebegin',['rangeBegin',['../classCabana_1_1LinkedCellList.html#a44a750305f79727a4982e729536d6558',1,'Cabana::LinkedCellList::rangeBegin()'],['../classCabana_1_1BinningData.html#aa76bd0a0c9620845968cecad43bbd5f7',1,'Cabana::BinningData::rangeBegin()']]],
+  ['rangeend',['rangeEnd',['../classCabana_1_1LinkedCellList.html#a0d8529a197a5fcbc0b7b7a10b4f1b478',1,'Cabana::LinkedCellList::rangeEnd()'],['../classCabana_1_1BinningData.html#af9e0811165d8a129d70ecde80101ac10',1,'Cabana::BinningData::rangeEnd()']]],
   ['rangepolicy',['RangePolicy',['../classCabana_1_1RangePolicy.html',1,'Cabana']]],
   ['rank',['rank',['../classCabana_1_1Slice.html#a58a24305f741a12df94483d1b261f64e',1,'Cabana::Slice::rank()'],['../structCabana_1_1SoA_3_01VectorLength_00_01MemberTypes_3_01Types_8_8_8_4_01_4.html#a11117bbec07890be0af8e55452a8c1d4',1,'Cabana::SoA&lt; VectorLength, MemberTypes&lt; Types...&gt; &gt;::rank()']]],
   ['reserve',['reserve',['../classCabana_1_1AoSoA.html#a82123aff64317235fcd54cba710573fc',1,'Cabana::AoSoA']]],
