f5e2d3660fb16617f1da2c11fea5ad99e5a0010b
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2019-01-09 20:12:28 +0000

    Documentation Update

--- doxygen/search/all_4.js
+++ doxygen/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['exportsize',['exportSize',['../classCabana_1_1CommunicationPlan.html#ad4b4da5d20dbac486b86158c64c5fd88',1,'Cabana::CommunicationPlan']]],
-  ['extent',['extent',['../classCabana_1_1Slice.html#a02892dfff4b08a0b3115769106241df8',1,'Cabana::Slice::extent()'],['../structCabana_1_1SoA_3_01MemberTypes_3_01Types_8_8_8_4_00_01VectorLength_01_4.html#a37626d85b6be83bc46f8127d0d16f291',1,'Cabana::SoA&lt; MemberTypes&lt; Types...&gt;, VectorLength &gt;::extent()']]]
+  ['extent',['extent',['../classCabana_1_1Slice.html#a02892dfff4b08a0b3115769106241df8',1,'Cabana::Slice::extent()'],['../structCabana_1_1SoA_3_01MemberTypes_3_01Types_8_8_8_4_00_01VectorLength_01_4.html#a15bcee1ed6cd4f8c94dad3d3f87f33d9',1,'Cabana::SoA&lt; MemberTypes&lt; Types...&gt;, VectorLength &gt;::extent()']]]
 ];
