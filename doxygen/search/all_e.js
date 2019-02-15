2c267b678da4bb8be5413a79bb04c477de233a5b
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2019-02-15 17:58:46 +0000

    Documentation Update

--- doxygen/search/all_e.js
+++ doxygen/search/all_e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['parallel_5finfo',['parallel_info',['../structparallel__info.html',1,'']]],
   ['performancetraits',['PerformanceTraits',['../classCabana_1_1Impl_1_1PerformanceTraits.html',1,'Cabana::Impl']]],
   ['permutation',['permutation',['../classCabana_1_1LinkedCellList.html#aa4e774dd528ba65d719b279c69ece7a5',1,'Cabana::LinkedCellList::permutation()'],['../classCabana_1_1BinningData.html#a880c2ba297ff84c95ff31abcddd409aa',1,'Cabana::BinningData::permutation()']]],
   ['pthread',['pthread',['../classTest_1_1pthread.html',1,'Test']]],
--- doxygen/search/classes_c.js
+++ doxygen/search/classes_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['parallel_5finfo',['parallel_info',['../structparallel__info.html',1,'']]],
   ['performancetraits',['PerformanceTraits',['../classCabana_1_1Impl_1_1PerformanceTraits.html',1,'Cabana::Impl']]],
   ['pthread',['pthread',['../classTest_1_1pthread.html',1,'Test']]]
 ];
