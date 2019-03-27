f67127d667b49b3bc9ee8c643fb4493fca822f2d
G. Chen <gchen@lanl.gov>
2019-03-27 17:23:52 -0600

    Added 05_parallel_for frotran example.

---
+++ core/fortran-example/tutorial/05_parallel_for/parallel_for_example.F90
@@ -0,0 +1,68 @@
+#include "veclen.h"
+
+SUBROUTINE initialization(part,num_part) BIND(C)
+  USE, INTRINSIC :: ISO_C_BINDING
+  implicit none
+  integer i,j,a,s
+  type, BIND(C) :: ptl_type      
+     real (C_DOUBLE) :: d0(veclen) 
+     real (C_FLOAT ) :: d1(veclen) 
+  end type ptl_type
+
+  type(ptl_type) :: part(*)
+  INTEGER(C_INT), VALUE :: num_part
+  INTEGER :: n_soa
+
+!Find out the number of soa in the aosoa
+  n_soa = (num_part-1)/veclen+1
+
+!Assign data to the aosoa values
+!Note that num_part<n_soa*veclen, we fill the multiple of veclen anyway
+do s = 1, n_soa
+   do a = 1,veclen
+      part(s)%d0(a) = 1.0
+      part(s)%d1(a) = 2.0
+   end do
+end do
+
+end SUBROUTINE initialization
+
+
+
+SUBROUTINE kernel_1(part,s,a) BIND(C)
+  USE, INTRINSIC :: ISO_C_BINDING
+  implicit none
+
+  type, BIND(C) :: ptl_type      
+     real (C_DOUBLE) :: d0(veclen) 
+     real (C_FLOAT ) :: d1(veclen) 
+  end type ptl_type
+
+  type(ptl_type) :: part(*)
+  INTEGER(C_INT), VALUE :: s,a
+
+
+!Assign data to the aosoa values 
+!index is shifted by 1 for the fortran convention
+  part(s+1)%d0(a+1) =  part(s+1)%d1(a+1) 
+
+end SUBROUTINE kernel_1
+
+SUBROUTINE kernel_2(part,s0,a0,s1,a1) BIND(C)
+  USE, INTRINSIC :: ISO_C_BINDING
+  implicit none
+
+  type, BIND(C) :: ptl_type      
+     real (C_DOUBLE) :: d0(0:veclen-1) !Here index range is the C convention
+     real (C_FLOAT ) :: d1(0:veclen-1) 
+  end type ptl_type
+
+  type(ptl_type) :: part(*)
+  INTEGER(C_INT), VALUE :: s0,a0,s1,a1
+
+  
+!Assign data to the aosoa values
+!note: index is not shifted by 1 as in kernel_1
+  part(s1)%d1(a1) =  part(s0)%d1(a0) 
+
+end SUBROUTINE kernel_2
