90b31d1c77ee0629cd0eb77cf077161c458d701a
Shane Patrick Fogerty - 322405 <sfogerty@lanl.gov>
2019-03-29 18:14:49 -0600

    add defs needed for examples

---
+++ core/example/solvers/example_definitions.h
@@ -0,0 +1,5 @@
+//Definitions specific to certain examples
+//Presumably these could be changed between different examples
+const int SPACE_DIM = 3;//No 1-D or 2-D implementations yet!
+const int INNER_ARRAY_SIZE = 8;
+constexpr double MADELUNG_NACL(-1.747564594633182);
