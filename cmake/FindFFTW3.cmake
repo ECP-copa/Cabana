df645d6418faafbbe7b0ff999c013c4d471bbbe4
Shane Patrick Fogerty - 322405 <sfogerty@lanl.gov>
2019-04-03 14:04:19 -0600

    cabana license for FindFFTW3.cmake

--- cmake/FindFFTW3.cmake
+++ cmake/FindFFTW3.cmake
@@ -1,24 +1,13 @@
-# - Find fftw3
-# Find the native FFTW3 headers and libraries.
-#
-#  FFTW3_INCLUDE_DIRS - where to find fftw3.h, etc.
-#  FFTW3_LIBRARIES    - List of libraries when using fftw3.
-#  FFTW3_FOUND        - True if fftw3 found.
-#
-# Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+############################################################################
+# Copyright (c) 2018-2019 by the Cabana authors                            #
+# All rights reserved.                                                     #
+#                                                                          #
+# This file is part of the Cabana library. Cabana is distributed under a   #
+# BSD 3-clause license. For the licensing terms see the LICENSE file in    #
+# the top-level directory.                                                 #
+#                                                                          #
+# SPDX-License-Identifier: BSD-3-Clause                                    #
+############################################################################
 
 find_package(PkgConfig)
 
