1db558ffa4c9120e3ba68462ec2b9f5120ccd961
Shane Patrick Fogerty - 322405 <sfogerty@lanl.gov>
2019-04-04 11:24:29 -0600

    add license

--- core/example/longrange/particles.cpp
+++ core/example/longrange/particles.cpp
@@ -1,3 +1,16 @@
+/****************************************************************************
+ * Copyright (c) 2018-2019 by the Cabana authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the Cabana library. Cabana is distributed under a   *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+//These initialize particles for a NaCl example structure
+
 #include "definitions.h"
 
 // function to initialize particles as a NaCl crystal of length crystal_size
--- core/example/longrange/spme.cpp
+++ core/example/longrange/spme.cpp
@@ -1,3 +1,14 @@
+/****************************************************************************
+ * Copyright (c) 2018-2019 by the Cabana authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the Cabana library. Cabana is distributed under a   *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
 #include "definitions.h"
 #include "spme.h"
 #include <cmath>
@@ -7,7 +18,6 @@
 #ifdef Cabana_ENABLE_Cuda
 #include <cufft.h>
 #include <cufftw.h>
-//#include 
 #else
 #include <fftw3.h>
 #endif
