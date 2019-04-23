e1945559bd716f0abf5a838421209048831cea4f
G. Chen <gchen@lanl.gov>
2019-04-23 12:02:37 -0600

    Add copyright to Fortran files

--- core/fortran-example/tutorial/01_hello_world/hello_world.f90
+++ core/fortran-example/tutorial/01_hello_world/hello_world.f90
@@ -1,3 +1,14 @@
+! ****************************************************************************
+! * Copyright (c) 2018-2019 by the Cabana authors                            *
+! * All rights reserved.                                                     *
+! *                                                                          *
+! * This file is part of the Cabana library. Cabana is distributed under a   *
+! * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+! * the top-level directory.                                                 *
+! *                                                                          *
+! * SPDX-License-Identifier: BSD-3-Clause                                    *
+! ****************************************************************************
+
 SUBROUTINE print_hello_world () BIND(C)
   USE, INTRINSIC :: ISO_C_BINDING
   print *,"Hello World from Cabana (Fortran)!"
