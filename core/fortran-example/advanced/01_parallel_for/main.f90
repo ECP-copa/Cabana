47724961e7f48e967aead6a47af6b920eab09167
G. Chen <gchen@lanl.gov>
2019-05-06 17:30:14 -0600

    Add copyright info. in 01 example

--- core/fortran-example/advanced/01_parallel_for/main.f90
+++ core/fortran-example/advanced/01_parallel_for/main.f90
@@ -1,3 +1,14 @@
+! ****************************************************************************
+! * Copyright (c) 2018-2019 by the Cabana authors                            *
+! * All rights reserved.                                                     *
+! *                                                                          *
+! * This file is part of the Cabana library. Cabana is distributed under a   *
+! * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+! * the top-level directory.                                                 *
+! *                                                                          *
+! * SPDX-License-Identifier: BSD-3-Clause                                    *
+! ****************************************************************************
+
 program main
   use kokkos_fortran_wrapper, only : kokkos_initialize, kokkos_finalize
   use parallel_for_example_module, only : array_setup, parallel_for_example, array_deallocation
