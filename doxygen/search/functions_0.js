3c053d7309226c115df4cb8c82210761a121c94f
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2019-05-09 20:12:10 +0000

    Documentation Update

--- doxygen/search/functions_0.js
+++ doxygen/search/functions_0.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['a',['a',['../classCabana_1_1Impl_1_1Index.html#a756a0f9c34351b93860ce89de8189ee3',1,'Cabana::Impl::Index']]],
   ['access',['access',['../classCabana_1_1AoSoA.html#a297f230d8764cfb9c4610acc076918a9',1,'Cabana::AoSoA']]],
-  ['aosoa',['AoSoA',['../classCabana_1_1AoSoA.html#aaf8315a02aafaa6b7af0b7fe8b5a3080',1,'Cabana::AoSoA::AoSoA()'],['../classCabana_1_1AoSoA.html#afe8403af8e3f14ad140c1a27689a8f98',1,'Cabana::AoSoA::AoSoA(const int n)']]],
+  ['aosoa',['AoSoA',['../classCabana_1_1AoSoA.html#aaf8315a02aafaa6b7af0b7fe8b5a3080',1,'Cabana::AoSoA::AoSoA()'],['../classCabana_1_1AoSoA.html#a0a95dabaaad6014ca53fef9fb4ab00a8',1,'Cabana::AoSoA::AoSoA(const std::size_t n)']]],
   ['arraybegin',['arrayBegin',['../classCabana_1_1SimdPolicy.html#aaf8cc7113adc760fa3543bf9d5c06600',1,'Cabana::SimdPolicy']]],
   ['arraysize',['arraySize',['../classCabana_1_1AoSoA.html#a7603d8a3adfbb46242d9ae4c5e8afc28',1,'Cabana::AoSoA::arraySize()'],['../classCabana_1_1Slice.html#a76e2c3bf749f2f305bd09f2f762162f9',1,'Cabana::Slice::arraySize()']]]
 ];
