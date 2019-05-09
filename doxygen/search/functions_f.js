7a67dead9f92966844797c6c5f49be30320ca29e
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2019-05-09 15:19:12 +0000

    Documentation Update

--- doxygen/search/functions_f.js
+++ doxygen/search/functions_f.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['verletlist',['VerletList',['../classCabana_1_1VerletList.html#acd8a008ac9244bd6bb9ad6cea000a7b9',1,'Cabana::VerletList']]]
+  ['verletlist',['VerletList',['../classCabana_1_1VerletList.html#acd8a008ac9244bd6bb9ad6cea000a7b9',1,'Cabana::VerletList']]],
+  ['view',['view',['../classCabana_1_1Slice.html#afa3dd945f4a5f35c1462dfb9c132a6b2',1,'Cabana::Slice']]]
 ];
