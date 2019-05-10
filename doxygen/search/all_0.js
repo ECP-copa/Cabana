c06ec126847681a3b18d13b8bb38996a13294197
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2019-05-10 20:33:56 +0000

    Documentation Update

--- doxygen/search/all_0.js
+++ doxygen/search/all_0.js
@@ -3,7 +3,7 @@ var searchData=
   ['a',['a',['../classCabana_1_1Impl_1_1Index.html#a756a0f9c34351b93860ce89de8189ee3',1,'Cabana::Impl::Index']]],
   ['access',['access',['../classCabana_1_1AoSoA.html#a5ee5f40747c38e58b8e9926ddd9138b2',1,'Cabana::AoSoA']]],
   ['aosoa',['AoSoA',['../classCabana_1_1AoSoA.html',1,'Cabana']]],
-  ['aosoa',['AoSoA',['../classCabana_1_1AoSoA.html#a26cf11d8b92bda3aa07bf8804db322a2',1,'Cabana::AoSoA::AoSoA()'],['../classCabana_1_1AoSoA.html#a23a7fe06fe7a13c5c701ef3c18ab2dd4',1,'Cabana::AoSoA::AoSoA(const std::size_t n)'],['../classCabana_1_1AoSoA.html#a2bc0437ebc048ba3d54dd155739c272b',1,'Cabana::AoSoA::AoSoA(soa_type *ptr, const int num_soa, const int n)']]],
+  ['aosoa',['AoSoA',['../classCabana_1_1AoSoA.html#ac952483cd2ef4aa12b637aee1da26303',1,'Cabana::AoSoA::AoSoA(const std::string &amp;label=&quot;&quot;)'],['../classCabana_1_1AoSoA.html#a9206cddc345e68926c38fe66137ec0f9',1,'Cabana::AoSoA::AoSoA(const std::size_t n)'],['../classCabana_1_1AoSoA.html#ac9abb9ed5494fcc0e11fc4c8b98be16c',1,'Cabana::AoSoA::AoSoA(const std::string label, const std::size_t n)'],['../classCabana_1_1AoSoA.html#a2bc0437ebc048ba3d54dd155739c272b',1,'Cabana::AoSoA::AoSoA(soa_type *ptr, const int num_soa, const int n)']]],
   ['arraybegin',['arrayBegin',['../classCabana_1_1SimdPolicy.html#aaf8cc7113adc760fa3543bf9d5c06600',1,'Cabana::SimdPolicy']]],
   ['arraysize',['arraySize',['../classCabana_1_1AoSoA.html#a49ce1fd5892394c3a10c95cfe260551b',1,'Cabana::AoSoA::arraySize()'],['../classCabana_1_1Slice.html#a76e2c3bf749f2f305bd09f2f762162f9',1,'Cabana::Slice::arraySize()']]],
   ['assignmentop',['AssignmentOp',['../classTest_1_1AssignmentOp.html',1,'Test']]],
--- doxygen/search/all_a.js
+++ doxygen/search/all_a.js
@@ -1,8 +1,9 @@
 var searchData=
 [
+  ['label',['label',['../classCabana_1_1AoSoA.html#aa91ef263880ae97a87ed2ac12f113379',1,'Cabana::AoSoA::label()'],['../classCabana_1_1Slice.html#aa34a74a1c557aae7819d690a8bf88634',1,'Cabana::Slice::label()']]],
   ['layoutcabanaslice',['LayoutCabanaSlice',['../structKokkos_1_1LayoutCabanaSlice.html',1,'Kokkos']]],
-  ['linkedcelllist',['LinkedCellList',['../classCabana_1_1LinkedCellList.html',1,'Cabana']]],
   ['linkedcelllist',['LinkedCellList',['../classCabana_1_1LinkedCellList.html#a10a69ae7a175576ac02e6c29c8e4ac45',1,'Cabana::LinkedCellList::LinkedCellList()'],['../classCabana_1_1LinkedCellList.html#ad71abd9014f45358bb03406601c3099c',1,'Cabana::LinkedCellList::LinkedCellList(SliceType positions, const typename SliceType::value_type grid_delta[3], const typename SliceType::value_type grid_min[3], const typename SliceType::value_type grid_max[3], typename std::enable_if&lt;(is_slice&lt; SliceType &gt;::value), int &gt;::type *=0)'],['../classCabana_1_1LinkedCellList.html#a8c4cc7be428dbb3888abc7dcf2e1a780',1,'Cabana::LinkedCellList::LinkedCellList(SliceType positions, const std::size_t begin, const std::size_t end, const typename SliceType::value_type grid_delta[3], const typename SliceType::value_type grid_min[3], const typename SliceType::value_type grid_max[3], typename std::enable_if&lt;(is_slice&lt; SliceType &gt;::value), int &gt;::type *=0)']]],
+  ['linkedcelllist',['LinkedCellList',['../classCabana_1_1LinkedCellList.html',1,'Cabana']]],
   ['linkedcelllist_3c_20memory_5fspace_20_3e',['LinkedCellList&lt; memory_space &gt;',['../classCabana_1_1LinkedCellList.html',1,'Cabana']]],
   ['linkedcellstencil',['LinkedCellStencil',['../structCabana_1_1Impl_1_1LinkedCellStencil.html',1,'Cabana::Impl']]],
   ['linkedcellstencil_3c_20positionvaluetype_20_3e',['LinkedCellStencil&lt; PositionValueType &gt;',['../structCabana_1_1Impl_1_1LinkedCellStencil.html',1,'Cabana::Impl']]],
