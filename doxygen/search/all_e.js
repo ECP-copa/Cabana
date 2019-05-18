e82211c456ad89e3b8118f30e783ce382e64d9d8
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2019-05-18 02:25:41 +0000

    Documentation Update

--- doxygen/search/all_e.js
+++ doxygen/search/all_e.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['parallel_5finfo',['parallel_info',['../structparallel__info.html',1,'']]],
+  ['paralleltimer',['ParallelTimer',['../classParallelTimer.html',1,'']]],
   ['performancetraits',['PerformanceTraits',['../classCabana_1_1Impl_1_1PerformanceTraits.html',1,'Cabana::Impl']]],
   ['permutation',['permutation',['../classCabana_1_1LinkedCellList.html#aa4e774dd528ba65d719b279c69ece7a5',1,'Cabana::LinkedCellList::permutation()'],['../classCabana_1_1BinningData.html#a880c2ba297ff84c95ff31abcddd409aa',1,'Cabana::BinningData::permutation()']]],
   ['ptr',['ptr',['../classCabana_1_1AoSoA.html#af491865908460c92c5c1dce89797c9d7',1,'Cabana::AoSoA']]]
--- doxygen/search/classes_c.js
+++ doxygen/search/classes_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['parallel_5finfo',['parallel_info',['../structparallel__info.html',1,'']]],
+  ['paralleltimer',['ParallelTimer',['../classParallelTimer.html',1,'']]],
   ['performancetraits',['PerformanceTraits',['../classCabana_1_1Impl_1_1PerformanceTraits.html',1,'Cabana::Impl']]]
 ];
