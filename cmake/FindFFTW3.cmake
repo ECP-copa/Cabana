9a0b5899b2de761ec1982d3ae5798b3dc8e7322a
Christoph Junghans <junghans@lanl.gov>
2019-05-22 06:30:03 -0600

    cmake: use FFTW3 imported target

--- cmake/FindFFTW3.cmake
+++ cmake/FindFFTW3.cmake
@@ -25,9 +25,6 @@ find_path(FFTW3_INCLUDE_DIR fftw3.h HINTS ${PC_FFTW3_INCLUDE_DIRS})
 
 find_library(FFTW3_LIBRARY NAMES fftw3 HINTS ${PC_FFTW3_LIBRARY_DIRS} )
 
-set(FFTW3_LIBRARIES ${FFTW3_LIBRARY} )
-set(FFTW3_INCLUDE_DIRS ${FFTW3_INCLUDE_DIR} )
-
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set FFTW3_FOUND to TRUE
 # if all listed variables are TRUE
@@ -37,10 +34,23 @@ find_package_handle_standard_args(FFTW3 DEFAULT_MSG FFTW3_LIBRARY FFTW3_INCLUDE_
 if (FFTW3_FOUND)
   include(CheckLibraryExists)
   #adding MATH_LIBRARIES here to allow static libs, this does not harm us as we are anyway using it
-  check_library_exists("${FFTW3_LIBRARIES};${MATH_LIBRARIES}" fftw_plan_r2r_1d "" FOUND_FFTW_PLAN)
+  check_library_exists("${FFTW3_LIBRARY};${MATH_LIBRARIES}" fftw_plan_r2r_1d "" FOUND_FFTW_PLAN)
   if(NOT FOUND_FFTW_PLAN)
     message(FATAL_ERROR "Could not find fftw_plan_r2r_1d in ${FFTW3_LIBRARY}, take a look at the error message in ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log to find out what was going wrong. If you are using a static lib (.a) make sure you have specified all dependencies of fftw3 in FFTW3_LIBRARY by hand (i.e. -DFFTW3_LIBRARY='/path/to/libfftw3.so;/path/to/libm.so') !")
   endif(NOT FOUND_FFTW_PLAN)
 endif (FFTW3_FOUND)
+
+# Copy the results to the output variables and target.
+if(FFTW3_FOUND)
+  set(FFTW3_LIBRARIES ${FFTW3_LIBRARY} )
+  set(FFTW3_INCLUDE_DIRS ${FFTW3_INCLUDE_DIR} )
+
+  if(NOT TARGET FFTW3::FFTW3)
+    add_library(FFTW3::FFTW3 UNKNOWN IMPORTED)
+    set_target_properties(FFTW3::FFTW3 PROPERTIES
+      IMPORTED_LOCATION "${FFTW3_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${FFTW3_INCLUDE_DIRS}")
+  endif()
+endif()
 
 mark_as_advanced(FFTW3_INCLUDE_DIR FFTW3_LIBRARY )
