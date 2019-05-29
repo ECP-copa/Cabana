85f875a0cafdd5e2a55acf362559216e96d4631c
Robert Bird <bird@lanl.gov>
2019-05-29 15:44:25 -0600

    added simple example of unmanaged view that comes from malloc/new'd data, is populated, and then sorted

---
+++ core/example/tutorial/14_unmanaged_view/parallel_for_example.cpp
@@ -0,0 +1,137 @@
+/****************************************************************************
+ * Copyright (c) 2018-2019 by the Cabana authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the Cabana library. Cabana is distributed under a   *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#include <Cabana_Core.hpp>
+#include <Cabana_Sort.hpp>
+
+#include <cassert>
+
+//---------------------------------------------------------------------------//
+// parallel for using an unmanaged view.
+//---------------------------------------------------------------------------//
+void parallelForExample()
+{
+    /* Declare general run parameters */
+    using MemorySpace = Kokkos::HostSpace;
+    using ExecutionSpace = Kokkos::DefaultHostExecutionSpace;
+    using DeviceType = Kokkos::Device<ExecutionSpace,MemorySpace>;
+    const int VectorLength = 8;
+
+    /*
+     * Declare Underlying data type
+    */
+
+    // Regular class form
+    class Data {
+        public:
+            double a[VectorLength];
+            double b[VectorLength];
+    };
+
+    // Cabana MemberTypes form:
+    using DataTypes = Cabana::MemberTypes<double,double>;
+
+
+    /*
+     * Create the AoSoA.
+    */
+    const int num_tuple = 128;
+    const int num_soa = 128/8; // Be careful with the int division..
+
+    Data* local_data = new Data[num_tuple];
+
+    // This is equivalent to a Cabana AoAoS, which looks like:
+    //    Cabana::AoSoA<DataTypes,DeviceType,VectorLength> aosoa( "my_aosoa", num_tuple );
+
+    /*
+     * Populate user data.
+    */
+    for (int i = 0; i < num_soa; i++)
+    {
+        for (int j = 0; j < VectorLength; j++)
+        {
+            local_data[i].a[j] = i;
+            local_data[i].b[j] = j;
+        }
+    }
+
+    /*
+    * Convert the user allocated memory into an unmanaged AoSoA
+    */
+
+    // Define the underlying AoSoA type to hold the unmanaged data
+    using AoSoA_t = Cabana::AoSoA<
+        DataTypes,
+        MemorySpace,
+        VectorLength,
+        Kokkos::MemoryUnmanaged
+    >;
+
+    // Do convert
+    auto ptr = reinterpret_cast<typename AoSoA_t::soa_type*>(local_data);
+    AoSoA_t aosoa( ptr, num_soa, num_tuple );
+
+    /*
+    * Inspect Data In the Unmanaged AoSoA
+    */
+    auto slice_a = aosoa.slice<0>(); // a = 0
+    auto slice_b = aosoa.slice<1>(); // b = 1
+
+    // Look at the data in the AosoA
+    for (int i = 0; i < num_tuple; i++)
+    {
+        if ( slice_a(i) != i / VectorLength )
+        {
+            // Unexpected Value
+            printf("%d: Unexpected %e != %e \n", __LINE__, slice_a(i), i / VectorLength);
+        }
+        if ( slice_b(i) != i % VectorLength )
+        {
+            // Unexpected Value
+            printf("%d: Unexpected %e != %e \n", __LINE__, slice_b(i), i % VectorLength);
+        }
+    }
+
+    /*
+    * Use the view in an algorithm, like sort
+    */
+    auto binning_data = Cabana::sortByKey( slice_b );
+    Cabana::permute( binning_data, aosoa );
+
+    // Check it worked
+    for (int i = 1; i < num_tuple; i++)
+    {
+        // We expect it to be monotonically increasing
+        if (!(slice_b(i-1) >= slice_b(i)))
+        {
+            // Unexpected value
+            printf("%d: Unexpected %e vs %e \n", __LINE__, slice_b(i-1) , slice_b(i) );
+        }
+    }
+
+    // Clean up local data
+    delete local_data;
+
+}
+
+//---------------------------------------------------------------------------//
+// Main.
+//---------------------------------------------------------------------------//
+int main( int argc, char* argv[] )
+{
+    Kokkos::ScopeGuard scope_guard(argc, argv);
+
+    parallelForExample();
+
+    return 0;
+}
+
+//---------------------------------------------------------------------------//
