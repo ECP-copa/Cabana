225d4e3fee8946e1716910e4c58cee3fdd4ab0bb
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2019-07-30 13:21:48 +0000

    Documentation Update

--- doxygen/search/all_2.js
+++ doxygen/search/all_2.js
@@ -16,6 +16,6 @@ var searchData=
   ['countsendsandcreatesteeringatomic',['CountSendsAndCreateSteeringAtomic',['../structCabana_1_1Impl_1_1CountSendsAndCreateSteeringAtomic.html',1,'Cabana::Impl']]],
   ['countsendsandcreatesteeringduplicated',['CountSendsAndCreateSteeringDuplicated',['../structCabana_1_1Impl_1_1CountSendsAndCreateSteeringDuplicated.html',1,'Cabana::Impl']]],
   ['createexportsteering',['createExportSteering',['../classCabana_1_1CommunicationPlan.html#a8a8281c28110645e3a8af293a1984917',1,'Cabana::CommunicationPlan::createExportSteering(const PackViewType &amp;neighbor_ids, const RankViewType &amp;element_export_ranks)'],['../classCabana_1_1CommunicationPlan.html#aa0809d8f8ef5a6594f00cf7b1ed50fec',1,'Cabana::CommunicationPlan::createExportSteering(const PackViewType &amp;neighbor_ids, const RankViewType &amp;element_export_ranks, const IdViewType &amp;element_export_ids)']]],
-  ['createfromexportsandtopology',['createFromExportsAndTopology',['../classCabana_1_1CommunicationPlan.html#a4b917c46cf274f309969ae4c05ef8120',1,'Cabana::CommunicationPlan']]],
-  ['createfromexportsonly',['createFromExportsOnly',['../classCabana_1_1CommunicationPlan.html#a370578437d000d1181576fc9e682889b',1,'Cabana::CommunicationPlan']]]
+  ['createfromexportsandtopology',['createFromExportsAndTopology',['../classCabana_1_1CommunicationPlan.html#ad976f9f24aee8d1023d2666b76d83e1d',1,'Cabana::CommunicationPlan']]],
+  ['createfromexportsonly',['createFromExportsOnly',['../classCabana_1_1CommunicationPlan.html#a241450ab2e00b530d5909b1c32e2f756',1,'Cabana::CommunicationPlan']]]
 ];
--- doxygen/search/functions_5.js
+++ doxygen/search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['getexportsteering',['getExportSteering',['../classCabana_1_1CommunicationPlan.html#a14b93e676802d1d6358a843af9a954a4',1,'Cabana::CommunicationPlan']]],
+  ['getexportsteering',['getExportSteering',['../classCabana_1_1CommunicationPlan.html#a05423e62ce1bdc7328abdce62298a234',1,'Cabana::CommunicationPlan']]],
   ['gettuple',['getTuple',['../classCabana_1_1AoSoA.html#ad60b044fe90293b911f7318c320f6f55',1,'Cabana::AoSoA']]]
 ];
