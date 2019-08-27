39e4358675c0762312043a63c28724c38b0885ef
Rene Halver <r.halver@fz-juelich.de>
2019-08-27 15:12:11 -0600

    parallel, distributed creation of the NaCl crystal added

--- core/example/longrange/particles.cpp
+++ core/example/longrange/particles.cpp
@@ -12,6 +12,7 @@
 // These initialize particles for a NaCl example structure
 
 #include "definitions.h"
+#include "mpi.h"
 
 // function to initialize particles as a NaCl crystal of length crystal_size
 void initializeParticles( ParticleList particles, int crystal_size )
@@ -54,6 +55,79 @@ void initializeParticles( ParticleList particles, int crystal_size )
     };
     Kokkos::parallel_for(
         Kokkos::RangePolicy<ExecutionSpace>( 0, particles.size() ),
+        init_parts );
+    Kokkos::fence();
+}
+
+// function to initialize particles as a NaCl crystal of length crystal_size
+// MPI parallel version
+void initializeParticles( ParticleList *particles, int c_size,
+                          std::vector<int> glob_dims,
+                          Kokkos::View<double *> domain_size,
+                          std::vector<int> loc_coords )
+{
+    // calculate the number of particles on the local domain
+    Kokkos::View<int *> indices( "indices for particle creation", 6 );
+    Kokkos::View<int *> loc_edges( "indices for particle creation", 3 );
+    int n_particles = 1;
+
+    for ( int dim = 0; dim < 3; ++dim )
+    {
+        indices( 2 * dim ) =
+            (int)std::ceil( loc_coords.at( dim ) * domain_size( dim ) / 0.5 );
+        indices( 2 * dim + 1 ) = (int)std::ceil( ( loc_coords.at( dim ) + 1 ) *
+                                                 domain_size( dim ) / 0.5 );
+        if ( glob_dims.at( dim ) == loc_coords.at( dim ) + 1 ||
+             fabs( ( loc_coords.at( dim ) + 1 ) * domain_size( dim ) / 0.5 -
+                   (int)( ( loc_coords.at( dim ) + 1 ) * domain_size( dim ) /
+                          0.5 ) ) < 1e-6 )
+            indices( 2 * dim + 1 ) = indices( 2 * dim + 1 ) - 1;
+        loc_edges( dim ) = indices( 2 * dim + 1 ) - indices( 2 * dim ) + 1;
+        n_particles *= loc_edges( dim );
+    }
+
+    int rank = loc_coords.at( 0 );
+
+    particles->resize( n_particles );
+
+    auto x = Cabana::slice<Position>( *particles );
+    auto v = Cabana::slice<Velocity>( *particles );
+    auto f = Cabana::slice<Force>( *particles );
+    auto q = Cabana::slice<Charge>( *particles );
+    auto u = Cabana::slice<Potential>( *particles );
+    auto i = Cabana::slice<Index>( *particles );
+    auto init_parts = KOKKOS_LAMBDA( const int idx )
+    {
+        // Calculate location of particle in crystal
+        int idx_z = idx % loc_edges( 2 ) + indices( 4 );
+        int idx_y = ( idx / loc_edges( 2 ) ) % loc_edges( 1 ) + indices( 2 );
+        int idx_x = idx / ( loc_edges( 1 ) * loc_edges( 2 ) ) + indices( 0 );
+
+        // Initialize position.
+        x( idx, 0 ) = ( (double)idx_x * 0.5 );
+        x( idx, 1 ) = ( (double)idx_y * 0.5 );
+        x( idx, 2 ) = ( (double)idx_z * 0.5 );
+
+        // Initialize velocity.
+        for ( int d = 0; d < SPACE_DIM; ++d )
+            v( idx, d ) = 0.0;
+
+        // Initialize field
+        for ( int d = 0; d < SPACE_DIM; ++d )
+            f( idx, d ) = 0.0;
+
+        // Create alternating charge
+        q( idx ) = ( ( ( idx_x + idx_y + idx_z ) % 2 ) ? 1.0 : -1.0 ) *
+                   COULOMB_PREFACTOR_INV;
+
+        // Set potential
+        u( idx ) = 0.0;
+
+        // Set global particle index
+        i( idx ) = idx_z + idx_y * c_size + idx_x * c_size * c_size;
+    };
+    Kokkos::parallel_for(
+        Kokkos::RangePolicy<ExecutionSpace>( 0, particles->size() ),
         init_parts );
     Kokkos::fence();
 }
