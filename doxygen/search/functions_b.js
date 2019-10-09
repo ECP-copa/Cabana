4ea317926d749f6e35066dcb5f17403c1f64eb81
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2019-10-09 20:10:25 +0000

    Documentation Update

--- doxygen/search/functions_b.js
+++ doxygen/search/functions_b.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['permutation',['permutation',['../classCabana_1_1LinkedCellList.html#af64240f1d5e026260208f6da8881d265',1,'Cabana::LinkedCellList::permutation()'],['../classCabana_1_1BinningData.html#a0653273d16d4db110a87c2dcc2270bb3',1,'Cabana::BinningData::permutation()']]],
-  ['ptr',['ptr',['../classCabana_1_1AoSoA.html#a65f606764fe4f17db7ec1dbea2c6caca',1,'Cabana::AoSoA']]]
+  ['permutation',['permutation',['../classCabana_1_1LinkedCellList.html#af64240f1d5e026260208f6da8881d265',1,'Cabana::LinkedCellList::permutation()'],['../classCabana_1_1BinningData.html#a0653273d16d4db110a87c2dcc2270bb3',1,'Cabana::BinningData::permutation()']]]
 ];
