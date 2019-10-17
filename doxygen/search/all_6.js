1edf0c091b36b5706d91776f159d179612064221
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2019-10-17 21:07:13 +0000

    Documentation Update

--- doxygen/search/all_6.js
+++ doxygen/search/all_6.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['getexportsteering',['getExportSteering',['../classCabana_1_1CommunicationPlan.html#a05423e62ce1bdc7328abdce62298a234',1,'Cabana::CommunicationPlan']]],
-  ['gettuple',['getTuple',['../classCabana_1_1AoSoA.html#ad60b044fe90293b911f7318c320f6f55',1,'Cabana::AoSoA']]],
-  ['ghostowningranks',['ghostOwningRanks',['../classCabana_1_1Halo.html#ab008c25b4821029732167e67d020460a',1,'Cabana::Halo']]]
+  ['gettuple',['getTuple',['../classCabana_1_1AoSoA.html#ad60b044fe90293b911f7318c320f6f55',1,'Cabana::AoSoA']]]
 ];
