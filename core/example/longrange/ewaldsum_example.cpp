28e4682dca6a6056f5051c77a6370a3201b4cbff
Shane Patrick Fogerty - 322405 <sfogerty@gmail.com>
2019-10-23 11:36:02 -0600

    clang-format 7.0

--- core/example/longrange/ewaldsum_example.cpp
+++ core/example/longrange/ewaldsum_example.cpp
@@ -141,24 +141,21 @@ int main( int argc, char **argv )
         // compute sum of forces (nicer with functor)
         double tfx, tfy, tfz;
         auto forces = Cabana::slice<Force>( *particles );
-        Kokkos::parallel_reduce(
-            particles->size(),
-            KOKKOS_LAMBDA( int idx, double &f_tmp ) {
-                f_tmp += forces( idx, 0 );
-            },
-            tfx );
-        Kokkos::parallel_reduce(
-            particles->size(),
-            KOKKOS_LAMBDA( int idx, double &f_tmp ) {
-                f_tmp += forces( idx, 1 );
-            },
-            tfy );
-        Kokkos::parallel_reduce(
-            particles->size(),
-            KOKKOS_LAMBDA( int idx, double &f_tmp ) {
-                f_tmp += forces( idx, 2 );
-            },
-            tfz );
+        Kokkos::parallel_reduce( particles->size(),
+                                 KOKKOS_LAMBDA( int idx, double &f_tmp ) {
+                                     f_tmp += forces( idx, 0 );
+                                 },
+                                 tfx );
+        Kokkos::parallel_reduce( particles->size(),
+                                 KOKKOS_LAMBDA( int idx, double &f_tmp ) {
+                                     f_tmp += forces( idx, 1 );
+                                 },
+                                 tfy );
+        Kokkos::parallel_reduce( particles->size(),
+                                 KOKKOS_LAMBDA( int idx, double &f_tmp ) {
+                                     f_tmp += forces( idx, 2 );
+                                 },
+                                 tfz );
 
         double fx, fy, fz;
         MPI_Allreduce( &tfx, &fx, 1, MPI_DOUBLE, MPI_SUM, cart_comm );
