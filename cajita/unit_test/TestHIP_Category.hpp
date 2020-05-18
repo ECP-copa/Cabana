0890042c01088f5787a881b44fca1300f8b35298
Stuart Slattery <slatterysr@ornl.gov>
2020-05-18 08:52:37 -0400

    adding UVM, HIP, and THREADS testing to Cajita

---
+++ cajita/unit_test/TestHIP_Category.hpp
@@ -0,0 +1,20 @@
+/****************************************************************************
+ * Copyright (c) 2018-2020 by the Cabana authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the Cabana library. Cabana is distributed under a   *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#ifndef CABANA_TEST_HIP_CATEGORY_HPP
+#define CABANA_TEST_HIP_CATEGORY_HPP
+
+#define TEST_CATEGORY hip
+#define TEST_EXECSPACE Kokkos::Experimental::HIP
+#define TEST_MEMSPACE Kokkos::Experimental::HIPSpace
+#define TEST_DEVICE Kokkos::Device<Kokkos::Experimental::HIP,Kokkos::Experimental::HIPSpace>
+
+#endif
---
+++ cajita/unit_test/TestTHREADS_Category.hpp
@@ -0,0 +1,20 @@
+/****************************************************************************
+ * Copyright (c) 2018-2020 by the Cabana authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the Cabana library. Cabana is distributed under a   *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#ifndef CABANA_TEST_PTHREAD_CATEGORY_HPP
+#define CABANA_TEST_PTHREAD_CATEGORY_HPP
+
+#define TEST_CATEGORY pthread
+#define TEST_EXECSPACE Kokkos::Threads
+#define TEST_MEMSPACE Kokkos::HostSpace
+#define TEST_DEVICE Kokkos::Device<Kokkos::Threads,Kokkos::HostSpace>
+
+#endif // end CABANA_TEST_PTHREAD_CATEGORY_HPP
