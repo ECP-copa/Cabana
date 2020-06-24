1d7b4e93b8ff7757015747c10a855fc16cfdc3a0
YuxingQiu <yuxqiu@gmail.com>
2020-06-23 23:31:05 -0700

    add GlobalMesh - SparseMesh

--- cajita/src/Cajita_GlobalMesh.hpp
+++ cajita/src/Cajita_GlobalMesh.hpp
@@ -238,6 +238,154 @@ createNonUniformGlobalMesh( const std::vector<Scalar> &i_edges,
 }
 
 //---------------------------------------------------------------------------//
+// Global mesh partial specialization for sparse mesh. Sparse meshes are
+// rectilinear meshes where every cell in the mesh is identical. Only meshes 
+// that will be used is stored and managed. Every cell has the same size in
+// each corresponding dimension.
+template <class Scalar>
+class GlobalMesh<SparseMesh<Scalar>>
+{
+  public:
+    // Mesh type
+    using mesh_type = SparseMesh<Scalar>;
+
+    // Scalar type
+    using scalar_type = Scalar;
+
+    // Cell size constructor -- special case where all cell dimensions are the 
+    // same.
+    GlobalMesh( const std::array<Scalar, 3> &global_low_corner,
+                const std::array<Scalar, 3> &global_high_corner,
+                const Scalar cell_size )
+        : _global_low_corner( global_low_corner )
+        , _global_high_corner( global_high_corner )
+        , _cell_size( {cell_size, cell_size, cell_size} )
+    {
+        // Check that the domain is evenly divisible by the cell size in each
+        // dimension within round-off error.
+        for ( int d = 0; d < 3; ++d )
+        {
+            Scalar ext = globalNumCell( d ) * _cell_size[d];
+            if ( std::abs( ext - extent( d ) ) >
+                 Scalar( 100.0 ) * std::numeric_limits<Scalar>::epsilon() )
+                throw std::logic_error(
+                    "Extent not evenly divisible by uniform cell size" );
+        }
+    }
+
+    // Cell size constructor - each cell dimension can be different.
+    GlobalMesh( const std::array<Scalar, 3> &global_low_corner,
+                const std::array<Scalar, 3> &global_high_corner,
+                const std::array<Scalar, 3> &cell_size )
+        : _global_low_corner( global_low_corner )
+        , _global_high_corner( global_high_corner )
+        , _cell_size( cell_size )
+    {
+        // Check that the domain is evenly divisible by the cell size in each
+        // dimension within round-off error.
+        for ( int d = 0; d < 3; ++d )
+        {
+            Scalar ext = globalNumCell( d ) * _cell_size[d];
+            if ( std::abs( ext - extent( d ) ) >
+                 Scalar( 100.0 ) * std::numeric_limits<Scalar>::epsilon() )
+                throw std::logic_error(
+                    "Extent not evenly divisible by uniform cell size" );
+        }
+    }
+
+    // Number of global cells constructor.
+    GlobalMesh( const std::array<Scalar, 3> &global_low_corner,
+                const std::array<Scalar, 3> &global_high_corner,
+                const std::array<int, 3> &global_num_cell )
+        : _global_low_corner( global_low_corner )
+        , _global_high_corner( global_high_corner )
+    {
+        // Compute the cell size in each dimension.
+        for ( int d = 0; d < 3; ++d )
+            _cell_size[d] = ( _global_high_corner[d] - _global_low_corner[d] ) /
+                            global_num_cell[d];
+
+        // Check that the domain is evenly divisible by the cell size in each
+        // dimension within round-off error and that we got the expected
+        // number of cells.
+        for ( int d = 0; d < 3; ++d )
+        {
+            Scalar ext = globalNumCell( d ) * _cell_size[d];
+            if ( std::abs( ext - extent( d ) ) >
+                 Scalar( 100.0 ) * std::numeric_limits<Scalar>::epsilon() )
+                throw std::logic_error(
+                    "Extent not evenly divisible by uniform cell size" );
+            if ( globalNumCell( d ) != global_num_cell[d] )
+                throw std::logic_error( "Global number of cells mismatch" );
+        }
+    }
+
+    // GLOBAL MESH INTERFACE
+
+    // Get the global low corner of the mesh.
+    Scalar lowCorner( const int dim ) const { return _global_low_corner[dim]; }
+
+    // Get the global high corner of the mesh.
+    Scalar highCorner( const int dim ) const
+    {
+        return _global_high_corner[dim];
+    }
+
+    // Get the extent of a given dimension.
+    Scalar extent( const int dim ) const
+    {
+        return highCorner( dim ) - lowCorner( dim );
+    }
+
+    // Get the global numer of cells in a given dimension.
+    int globalNumCell( const int dim ) const
+    {
+        return std::rint( extent( dim ) / _cell_size[dim] );
+    }
+
+    // UNIFORM MESH SPECIFIC (Even Sparse)
+
+    // Get the uniform cell size in a given dimension.
+    Scalar cellSize( const int dim ) const { return _cell_size[dim]; }
+
+  private:
+    std::array<Scalar, 3> _global_low_corner;
+    std::array<Scalar, 3> _global_high_corner;
+    std::array<Scalar, 3> _cell_size;
+};
+
+// Creation functions
+template <class Scalar>
+std::shared_ptr<GlobalMesh<SparseMesh<Scalar>>>
+createSparseGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
+                        const std::array<Scalar, 3> &global_high_corner,
+                        const Scalar cell_size )
+{
+    return std::make_shared<GlobalMesh<UniformMesh<Scalar>>>(
+        global_low_corner, global_high_corner, cell_size );
+}
+
+template <class Scalar>
+std::shared_ptr<GlobalMesh<UniformMesh<Scalar>>>
+createSparseGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
+                        const std::array<Scalar, 3> &global_high_corner,
+                        const std::array<Scalar, 3> &cell_size )
+{
+    return std::make_shared<GlobalMesh<UniformMesh<Scalar>>>(
+        global_low_corner, global_high_corner, cell_size );
+}
+
+template <class Scalar>
+std::shared_ptr<GlobalMesh<UniformMesh<Scalar>>>
+createSparseGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
+                        const std::array<Scalar, 3> &global_high_corner,
+                        const std::array<int, 3> &global_num_cell )
+{
+    return std::make_shared<GlobalMesh<UniformMesh<Scalar>>>(
+        global_low_corner, global_high_corner, global_num_cell );
+}
+
+//---------------------------------------------------------------------------//
 
 } // end namespace Cajita
 
