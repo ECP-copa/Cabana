4dc55c38e97285d38265d2551f2680a24e5dfb8f
YuxingQiu <yuxqiu@gmail.com>
2020-06-24 10:38:42 -0700

    Generalize GlobalMesh for UniformMesh/SparseMesh

--- cajita/src/Cajita_GlobalMesh.hpp
+++ cajita/src/Cajita_GlobalMesh.hpp
@@ -31,21 +31,21 @@ class GlobalMesh;
 // Global mesh partial specialization for uniform mesh. Uniform meshes are
 // rectilinear meshes where every cell in the mesh is identical. A cell is
 // described by its width in each dimension.
-template <class Scalar>
-class GlobalMesh<UniformMesh<Scalar>>
+template <class MeshType>
+class GlobalMesh
 {
   public:
     // Mesh type.
-    using mesh_type = UniformMesh<Scalar>;
+    using mesh_type = MeshType;
 
     // Scalar type.
-    using scalar_type = Scalar;
+    using scalar_type = typename mesh_type::scalar_type;
 
     // Cell size constructor - special case where all cell dimensions are the
     // same.
-    GlobalMesh( const std::array<Scalar, 3> &global_low_corner,
-                const std::array<Scalar, 3> &global_high_corner,
-                const Scalar cell_size )
+    GlobalMesh( const std::array<scalar_type, 3> &global_low_corner,
+                const std::array<scalar_type, 3> &global_high_corner,
+                const scalar_type cell_size )
         : _global_low_corner( global_low_corner )
         , _global_high_corner( global_high_corner )
         , _cell_size( {cell_size, cell_size, cell_size} )
@@ -54,18 +54,18 @@ class GlobalMesh<UniformMesh<Scalar>>
         // dimension within round-off error.
         for ( int d = 0; d < 3; ++d )
         {
-            Scalar ext = globalNumCell( d ) * _cell_size[d];
+            scalar_type ext = globalNumCell( d ) * _cell_size[d];
             if ( std::abs( ext - extent( d ) ) >
-                 Scalar( 100.0 ) * std::numeric_limits<Scalar>::epsilon() )
+                 scalar_type( 100.0 ) * std::numeric_limits<scalar_type>::epsilon() )
                 throw std::logic_error(
                     "Extent not evenly divisible by uniform cell size" );
         }
     }
 
     // Cell size constructor - each cell dimension can be different.
-    GlobalMesh( const std::array<Scalar, 3> &global_low_corner,
-                const std::array<Scalar, 3> &global_high_corner,
-                const std::array<Scalar, 3> &cell_size )
+    GlobalMesh( const std::array<scalar_type, 3> &global_low_corner,
+                const std::array<scalar_type, 3> &global_high_corner,
+                const std::array<scalar_type, 3> &cell_size )
         : _global_low_corner( global_low_corner )
         , _global_high_corner( global_high_corner )
         , _cell_size( cell_size )
@@ -74,17 +74,17 @@ class GlobalMesh<UniformMesh<Scalar>>
         // dimension within round-off error.
         for ( int d = 0; d < 3; ++d )
         {
-            Scalar ext = globalNumCell( d ) * _cell_size[d];
+            scalar_type ext = globalNumCell( d ) * _cell_size[d];
             if ( std::abs( ext - extent( d ) ) >
-                 Scalar( 100.0 ) * std::numeric_limits<Scalar>::epsilon() )
+                 scalar_type( 100.0 ) * std::numeric_limits<scalar_type>::epsilon() )
                 throw std::logic_error(
                     "Extent not evenly divisible by uniform cell size" );
         }
     }
 
     // Number of global cells constructor.
-    GlobalMesh( const std::array<Scalar, 3> &global_low_corner,
-                const std::array<Scalar, 3> &global_high_corner,
+    GlobalMesh( const std::array<scalar_type, 3> &global_low_corner,
+                const std::array<scalar_type, 3> &global_high_corner,
                 const std::array<int, 3> &global_num_cell )
         : _global_low_corner( global_low_corner )
         , _global_high_corner( global_high_corner )
@@ -99,9 +99,9 @@ class GlobalMesh<UniformMesh<Scalar>>
         // number of cells.
         for ( int d = 0; d < 3; ++d )
         {
-            Scalar ext = globalNumCell( d ) * _cell_size[d];
+            scalar_type ext = globalNumCell( d ) * _cell_size[d];
             if ( std::abs( ext - extent( d ) ) >
-                 Scalar( 100.0 ) * std::numeric_limits<Scalar>::epsilon() )
+                 scalar_type( 100.0 ) * std::numeric_limits<scalar_type>::epsilon() )
                 throw std::logic_error(
                     "Extent not evenly divisible by uniform cell size" );
             if ( globalNumCell( d ) != global_num_cell[d] )
@@ -112,16 +112,16 @@ class GlobalMesh<UniformMesh<Scalar>>
     // GLOBAL MESH INTERFACE
 
     // Get the global low corner of the mesh.
-    Scalar lowCorner( const int dim ) const { return _global_low_corner[dim]; }
+    scalar_type lowCorner( const int dim ) const { return _global_low_corner[dim]; }
 
     // Get the global high corner of the mesh.
-    Scalar highCorner( const int dim ) const
+    scalar_type highCorner( const int dim ) const
     {
         return _global_high_corner[dim];
     }
 
     // Get the extent of a given dimension.
-    Scalar extent( const int dim ) const
+    scalar_type extent( const int dim ) const
     {
         return highCorner( dim ) - lowCorner( dim );
     }
@@ -135,15 +135,15 @@ class GlobalMesh<UniformMesh<Scalar>>
     // UNIFORM MESH SPECIFIC
 
     // Get the uniform cell size in a given dimension.
-    Scalar cellSize( const int dim ) const { return _cell_size[dim]; }
+    scalar_type cellSize( const int dim ) const { return _cell_size[dim]; }
 
   private:
-    std::array<Scalar, 3> _global_low_corner;
-    std::array<Scalar, 3> _global_high_corner;
-    std::array<Scalar, 3> _cell_size;
+    std::array<scalar_type, 3> _global_low_corner;
+    std::array<scalar_type, 3> _global_high_corner;
+    std::array<scalar_type, 3> _cell_size;
 };
 
-// Creation function.
+// Creation function for Uniform Mesh.
 template <class Scalar>
 std::shared_ptr<GlobalMesh<UniformMesh<Scalar>>>
 createUniformGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
@@ -171,6 +171,37 @@ createUniformGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
                          const std::array<int, 3> &global_num_cell )
 {
     return std::make_shared<GlobalMesh<UniformMesh<Scalar>>>(
+        global_low_corner, global_high_corner, global_num_cell );
+}
+
+// Creation functions for Sparse Mesh.
+template <class Scalar>
+std::shared_ptr<GlobalMesh<SparseMesh<Scalar>>>
+createSparseGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
+                        const std::array<Scalar, 3> &global_high_corner,
+                        const Scalar cell_size )
+{
+    return std::make_shared<GlobalMesh<SparseMesh<Scalar>>>(
+        global_low_corner, global_high_corner, cell_size );
+}
+
+template <class Scalar>
+std::shared_ptr<GlobalMesh<SparseMesh<Scalar>>>
+createSparseGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
+                        const std::array<Scalar, 3> &global_high_corner,
+                        const std::array<Scalar, 3> &cell_size )
+{
+    return std::make_shared<GlobalMesh<SparseMesh<Scalar>>>(
+        global_low_corner, global_high_corner, cell_size );
+}
+
+template <class Scalar>
+std::shared_ptr<GlobalMesh<SparseMesh<Scalar>>>
+createSparseGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
+                        const std::array<Scalar, 3> &global_high_corner,
+                        const std::array<int, 3> &global_num_cell )
+{
+    return std::make_shared<GlobalMesh<SparseMesh<Scalar>>>(
         global_low_corner, global_high_corner, global_num_cell );
 }
 
@@ -235,154 +266,6 @@ createNonUniformGlobalMesh( const std::vector<Scalar> &i_edges,
 {
     return std::make_shared<GlobalMesh<NonUniformMesh<Scalar>>>(
         i_edges, j_edges, k_edges );
-}
-
-//---------------------------------------------------------------------------//
-// Global mesh partial specialization for sparse mesh. Sparse meshes are
-// rectilinear meshes where every cell in the mesh is identical. Only meshes 
-// that will be used is stored and managed. Every cell has the same size in
-// each corresponding dimension.
-template <class Scalar>
-class GlobalMesh<SparseMesh<Scalar>>
-{
-  public:
-    // Mesh type
-    using mesh_type = SparseMesh<Scalar>;
-
-    // Scalar type
-    using scalar_type = Scalar;
-
-    // Cell size constructor -- special case where all cell dimensions are the 
-    // same.
-    GlobalMesh( const std::array<Scalar, 3> &global_low_corner,
-                const std::array<Scalar, 3> &global_high_corner,
-                const Scalar cell_size )
-        : _global_low_corner( global_low_corner )
-        , _global_high_corner( global_high_corner )
-        , _cell_size( {cell_size, cell_size, cell_size} )
-    {
-        // Check that the domain is evenly divisible by the cell size in each
-        // dimension within round-off error.
-        for ( int d = 0; d < 3; ++d )
-        {
-            Scalar ext = globalNumCell( d ) * _cell_size[d];
-            if ( std::abs( ext - extent( d ) ) >
-                 Scalar( 100.0 ) * std::numeric_limits<Scalar>::epsilon() )
-                throw std::logic_error(
-                    "Extent not evenly divisible by uniform cell size" );
-        }
-    }
-
-    // Cell size constructor - each cell dimension can be different.
-    GlobalMesh( const std::array<Scalar, 3> &global_low_corner,
-                const std::array<Scalar, 3> &global_high_corner,
-                const std::array<Scalar, 3> &cell_size )
-        : _global_low_corner( global_low_corner )
-        , _global_high_corner( global_high_corner )
-        , _cell_size( cell_size )
-    {
-        // Check that the domain is evenly divisible by the cell size in each
-        // dimension within round-off error.
-        for ( int d = 0; d < 3; ++d )
-        {
-            Scalar ext = globalNumCell( d ) * _cell_size[d];
-            if ( std::abs( ext - extent( d ) ) >
-                 Scalar( 100.0 ) * std::numeric_limits<Scalar>::epsilon() )
-                throw std::logic_error(
-                    "Extent not evenly divisible by uniform cell size" );
-        }
-    }
-
-    // Number of global cells constructor.
-    GlobalMesh( const std::array<Scalar, 3> &global_low_corner,
-                const std::array<Scalar, 3> &global_high_corner,
-                const std::array<int, 3> &global_num_cell )
-        : _global_low_corner( global_low_corner )
-        , _global_high_corner( global_high_corner )
-    {
-        // Compute the cell size in each dimension.
-        for ( int d = 0; d < 3; ++d )
-            _cell_size[d] = ( _global_high_corner[d] - _global_low_corner[d] ) /
-                            global_num_cell[d];
-
-        // Check that the domain is evenly divisible by the cell size in each
-        // dimension within round-off error and that we got the expected
-        // number of cells.
-        for ( int d = 0; d < 3; ++d )
-        {
-            Scalar ext = globalNumCell( d ) * _cell_size[d];
-            if ( std::abs( ext - extent( d ) ) >
-                 Scalar( 100.0 ) * std::numeric_limits<Scalar>::epsilon() )
-                throw std::logic_error(
-                    "Extent not evenly divisible by uniform cell size" );
-            if ( globalNumCell( d ) != global_num_cell[d] )
-                throw std::logic_error( "Global number of cells mismatch" );
-        }
-    }
-
-    // GLOBAL MESH INTERFACE
-
-    // Get the global low corner of the mesh.
-    Scalar lowCorner( const int dim ) const { return _global_low_corner[dim]; }
-
-    // Get the global high corner of the mesh.
-    Scalar highCorner( const int dim ) const
-    {
-        return _global_high_corner[dim];
-    }
-
-    // Get the extent of a given dimension.
-    Scalar extent( const int dim ) const
-    {
-        return highCorner( dim ) - lowCorner( dim );
-    }
-
-    // Get the global numer of cells in a given dimension.
-    int globalNumCell( const int dim ) const
-    {
-        return std::rint( extent( dim ) / _cell_size[dim] );
-    }
-
-    // UNIFORM MESH SPECIFIC (Even Sparse)
-
-    // Get the uniform cell size in a given dimension.
-    Scalar cellSize( const int dim ) const { return _cell_size[dim]; }
-
-  private:
-    std::array<Scalar, 3> _global_low_corner;
-    std::array<Scalar, 3> _global_high_corner;
-    std::array<Scalar, 3> _cell_size;
-};
-
-// Creation functions
-template <class Scalar>
-std::shared_ptr<GlobalMesh<SparseMesh<Scalar>>>
-createSparseGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
-                        const std::array<Scalar, 3> &global_high_corner,
-                        const Scalar cell_size )
-{
-    return std::make_shared<GlobalMesh<UniformMesh<Scalar>>>(
-        global_low_corner, global_high_corner, cell_size );
-}
-
-template <class Scalar>
-std::shared_ptr<GlobalMesh<UniformMesh<Scalar>>>
-createSparseGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
-                        const std::array<Scalar, 3> &global_high_corner,
-                        const std::array<Scalar, 3> &cell_size )
-{
-    return std::make_shared<GlobalMesh<UniformMesh<Scalar>>>(
-        global_low_corner, global_high_corner, cell_size );
-}
-
-template <class Scalar>
-std::shared_ptr<GlobalMesh<UniformMesh<Scalar>>>
-createSparseGlobalMesh( const std::array<Scalar, 3> &global_low_corner,
-                        const std::array<Scalar, 3> &global_high_corner,
-                        const std::array<int, 3> &global_num_cell )
-{
-    return std::make_shared<GlobalMesh<UniformMesh<Scalar>>>(
-        global_low_corner, global_high_corner, global_num_cell );
 }
 
 //---------------------------------------------------------------------------//
