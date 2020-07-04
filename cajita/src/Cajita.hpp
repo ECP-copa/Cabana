e74095daed1283338bc901c927cba185ae39c913
YuxingQiu <yuxqiu@gmail.com>
2020-07-04 01:33:55 -0700

    blk local index space, coord <=> offset transform

--- cajita/src/Cajita.hpp
+++ cajita/src/Cajita.hpp
@@ -27,6 +27,7 @@
 #include <Cajita_MpiTraits.hpp>
 #include <Cajita_Partitioner.hpp>
 #include <Cajita_ReferenceStructuredSolver.hpp>
+#include <Cajita_SparseIndexSpace.hpp>
 #include <Cajita_Splines.hpp>
 #include <Cajita_Types.hpp>
 #include <Cajita_UniformDimPartitioner.hpp>
--- cajita/src/Cajita_SparseIndexSpace.hpp
+++ cajita/src/Cajita_SparseIndexSpace.hpp
@@ -14,25 +14,109 @@ namespace Cajita
   \class SparseIndexSpace
   \brief Sparse index space, hierarchical structure (cell->block->whole domain)
  */
-template <long N>
+template <long N, int BlkSizePerDim = 4>
 class SparseIndexSpace
 {
-// Should support
-// insert 
-// query
-// query and insert
-// compare operations?
-// get min/max operations?
+  public:
+    //! Number of dimensions
+    static constexpr long Rank = N;
+    //! Number of cells inside each block per dim
+    static constexpr int BlockSizePerDim = BlkSizePerDim;
+    //! Total number of cells inside each block
+    static constexpr int BlockSizeTotal
+    {
+        int BS = 1;
+        for ( decltype( N ) curDim = 0; curDim < N; curDim++ )
+            BS *= BlockSize;
+        return BS;
+    }
 
-private:
-// hash table (blockId -> blockNo)
-// Valid block number
-// Valid block Ids
-// allocated size
+    SparseIndexSpace() {}
+
+    // Should support
+    // insert
+    // query
+    // query and insert
+    // compare operations?
+    // get min/max operations?
+
+  private:
+    // hash table (blockId -> blockNo)
+    // Valid block number
+    // Valid block Ids
+    // allocated size
 }; // end class SparseIndexSpace
 
-} // end namespace Cajita
+//---------------------------------------------------------------------------//
+/*!
+  \class SparseBlockLocalIndexSpace
+  \brief Block local index space, hierarchical structure (cell->block->whole
+  domain) \brief CellNo : uint64_t \brief CellId : (int...)
+ */
 
+template <long N, int BlkSizePerDim>
+class SparseBlockLocalIndexSpace
+{
+    // coord <=> offset computations
+    struct Coord2OffsetDim
+    {
+        template <typename Coord>
+        constexpr auto operator()( int dimNo, Coord &&i ) -> uint64_t
+        {
+            uint64_t result = (uint64_t)i;
+            for ( int i = 0; i < dimNo; i++ )
+                result *= BlkSizePerDim;
+            return result;
+        }
+    };
+
+    struct Offset2CoordDim
+    {
+        template <typename Coord>
+        constexpr auto operator()( int dimNo, Coord &i, const uint64_t &offset )
+            -> uint64_t
+        {
+            i = offset % BlkSizePerDim;
+            return static_cast<uint64_t>( offset / BlkSizePerDim );
+        }
+    };
+
+  protected:
+    template <typename Func, typename Coord>
+    static constexpr auto from_coord_impl( int &&dimNo, Coord &&i )
+    {
+        return Func()( dimNo, std::forward<Coord>( i ) );
+    }
+
+    template <typename Func, typename Coord, typename... Coords>
+    static constexpr auto from_coord_impl( int &&dimNo, Coord &&i,
+                                           Coords &&... is )
+    {
+        auto result = Func()( dimNo, std::forward<Coord>( i ) );
+        if ( dimNo + 1 < N )
+            result += from_coord_impl<Func>( dimNo + 1,
+                                             std::forward<Coords>( is )... );
+        return result;
+    }
+
+    template <typename Func, typename Key, typename Coord>
+    static constexpr void to_coord_impl( int &&dimNo, Key &&key, Coord &i )
+    {
+        Func()( dimNo, i, std::forward<Key>( key ) );
+    }
+
+    template <typename Func, typename Key, typename Coord, typename... Coords>
+    static constexpr void to_coord_impl( int &&dimNo, Key &&key, Coord &i,
+                                         Coords &... is )
+    {
+        Key newKey = Func()( dimNo, i, std::forward<Key>( key ) );
+        if ( dimNo + 1 < N )
+            to_coord_impl<Func>( dimNo + 1, std::forward<Key>( Newkey ),
+                                 is... );
+    }
+}
+
+} // end namespace Cajita
 
 //---------------------------------------------------------------------------//
 // execution policies
@@ -46,6 +130,4 @@ private:
 
 // appendDimension
 
-
-
-#endif ///< ! CAJITA_SPARSE_INDEXSPACE_HPP 
+#endif ///< ! CAJITA_SPARSE_INDEXSPACE_HPP
