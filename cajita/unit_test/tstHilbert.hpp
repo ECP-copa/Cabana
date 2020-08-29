ae5c60d7d104a0541b9b298228f96e1c236fd1a1
Jered Dominguez-Trujillo <jereddt@gmail.com>
2020-08-28 22:50:23 -0600

    Mirror views

--- cajita/unit_test/tstHilbert.hpp
+++ cajita/unit_test/tstHilbert.hpp
@@ -74,8 +74,7 @@ void LayoutHilbert2DSubviewTest()
     buff_type dev_view( "dev_view", HilbertArray.extent( 0 ),
                         HilbertArray.extent( 1 ), HilbertArray.extent( 2 ),
                         HilbertArray.extent( 3 ) );
-    buff_type::HostMirror host_view_hilbert =
-        Kokkos::create_mirror( dev_view );
+    buff_type::HostMirror host_view_hilbert = Kokkos::create_mirror( dev_view );
 
     Kokkos::deep_copy( dev_view, HilbertArray );
     Kokkos::deep_copy( host_view_hilbert, dev_view );
@@ -198,8 +197,7 @@ void LayoutHilbert2DArrayOpTest()
     buff_type dev_view( "dev_view", array->view().extent( 0 ),
                         array->view().extent( 1 ), array->view().extent( 2 ),
                         array->view().extent( 3 ) );
-    buff_type::HostMirror host_view =
-        Kokkos::create_mirror( dev_view );
+    buff_type::HostMirror host_view = Kokkos::create_mirror( dev_view );
 
     Kokkos::deep_copy( dev_view, array->view() );
     Kokkos::deep_copy( host_view, dev_view );
@@ -280,8 +278,7 @@ void LayoutHilbert2DArrayOpTest()
                            subarray->view().extent( 1 ),
                            subarray->view().extent( 2 ),
                            subarray->view().extent( 3 ) );
-    buff_type::HostMirror host_subview =
-        Kokkos::create_mirror( dev_subview );
+    buff_type::HostMirror host_subview = Kokkos::create_mirror( dev_subview );
 
     Kokkos::deep_copy( dev_subview, subarray->view() );
     Kokkos::deep_copy( host_subview, dev_subview );
@@ -589,8 +586,7 @@ void LayoutHilbert2DGatherTest( const Cajita::ManualPartitioner &partitioner,
     buff_type dev_view( "dev_view", array->view().extent( 0 ),
                         array->view().extent( 1 ), array->view().extent( 2 ),
                         array->view().extent( 3 ) );
-    buff_type::HostMirror host_view =
-        Kokkos::create_mirror( dev_view );
+    buff_type::HostMirror host_view = Kokkos::create_mirror( dev_view );
 
     Kokkos::deep_copy( dev_view, array->view() );
     Kokkos::deep_copy( host_view, dev_view );
