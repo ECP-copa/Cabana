bb9b5f27685b5ec4c71773f62b843530ff5e14cd
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2020-09-02 21:43:34 +0000

    Documentation Update

--- doxygen/search/classes_a.js
+++ doxygen/search/classes_a.js
@@ -5,6 +5,7 @@ var searchData=
   ['neighbordiscriminator_3c_20halfneighbortag_20_3e',['NeighborDiscriminator&lt; HalfNeighborTag &gt;',['../classCabana_1_1Impl_1_1NeighborDiscriminator_3_01HalfNeighborTag_01_4.html',1,'Cabana::Impl']]],
   ['neighbordiscriminatorcallback',['NeighborDiscriminatorCallback',['../structCabana_1_1Experimental_1_1Impl_1_1NeighborDiscriminatorCallback.html',1,'Cabana::Experimental::Impl']]],
   ['neighbordiscriminatorcallback2d_5ffirstpass',['NeighborDiscriminatorCallback2D_FirstPass',['../structCabana_1_1Experimental_1_1Impl_1_1NeighborDiscriminatorCallback2D__FirstPass.html',1,'Cabana::Experimental::Impl']]],
+  ['neighbordiscriminatorcallback2d_5ffirstpass_5fbufferoptimization',['NeighborDiscriminatorCallback2D_FirstPass_BufferOptimization',['../structCabana_1_1Experimental_1_1Impl_1_1NeighborDiscriminatorCallback2D__FirstPass__BufferOptimization.html',1,'Cabana::Experimental::Impl']]],
   ['neighbordiscriminatorcallback2d_5fsecondpass',['NeighborDiscriminatorCallback2D_SecondPass',['../structCabana_1_1Experimental_1_1Impl_1_1NeighborDiscriminatorCallback2D__SecondPass.html',1,'Cabana::Experimental::Impl']]],
   ['neighborlist',['NeighborList',['../classCabana_1_1NeighborList.html',1,'Cabana']]],
   ['neighborlist_3c_20experimental_3a_3acrsgraph_3c_20memoryspace_2c_20tag_20_3e_20_3e',['NeighborList&lt; Experimental::CrsGraph&lt; MemorySpace, Tag &gt; &gt;',['../classCabana_1_1NeighborList_3_01Experimental_1_1CrsGraph_3_01MemorySpace_00_01Tag_01_4_01_4.html',1,'Cabana']]],
