18e894d814cd4bc1fcf0d4cf743eb4951ce44727
Robert Bird <bird@lanl.gov>
2020-09-22 11:43:04 -0600

    undo file changes made during format that have nothing to do with this PR

--- core/performance_test/peakflops/common.h
+++ core/performance_test/peakflops/common.h
@@ -1,7 +1,5 @@
 /*
-  Note that the performance output on skylake can be about twice as the expected
-  flops/cycle. This is because the rdtsp() is off by about a factor of two on
-  this cpu.
+  Note that the performance output on skylake can be about twice as the expected flops/cycle. This is because the rdtsp() is off by about a factor of two on this cpu.
   TODO: remove rdtsp() and use flops/sec for measuring performance.
  */
 #ifndef PEAKFLOPS_COMMON_H
@@ -9,12 +7,10 @@
 
 #include <gtest/gtest.h>
 
-static inline unsigned long long rdtscp()
-{
-    unsigned long long u;
-    asm volatile( "rdtscp;shlq $32,%%rdx;orq %%rdx,%%rax;movq %%rax,%0"
-                  : "=q"( u )::"%rax", "%rdx", "%rcx" );
-    return u;
+static inline unsigned long long rdtscp() {
+  unsigned long long u;
+  asm volatile ("rdtscp;shlq $32,%%rdx;orq %%rdx,%%rax;movq %%rax,%0":"=q"(u)::"%rax", "%rdx", "%rcx");
+  return u;
 }
 
 #endif // guard
