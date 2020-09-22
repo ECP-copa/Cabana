6e56f431e8803ecdb1fc71f3e730b99f59a3bfcc
Automatic Deployment (Travis CI) <noreply@lanl.gov>
2020-09-22 13:42:17 +0000

    Documentation Update

--- doxygen/menudata.js
+++ doxygen/menudata.js
@@ -21,8 +21,7 @@ var menudata={children:[
 {text:"r",url:"functions.html#index_r"},
 {text:"s",url:"functions.html#index_s"},
 {text:"t",url:"functions.html#index_t"},
-{text:"v",url:"functions.html#index_v"},
-{text:"~",url:"functions.html#index_0x7e"}]},
+{text:"v",url:"functions.html#index_v"}]},
 {text:"Functions",url:"functions_func.html",children:[
 {text:"a",url:"functions_func.html#index_a"},
 {text:"b",url:"functions_func.html#index_b"},
@@ -39,7 +38,6 @@ var menudata={children:[
 {text:"r",url:"functions_func.html#index_r"},
 {text:"s",url:"functions_func.html#index_s"},
 {text:"t",url:"functions_func.html#index_t"},
-{text:"v",url:"functions_func.html#index_v"},
-{text:"~",url:"functions_func.html#index_0x7e"}]}]}]},
+{text:"v",url:"functions_func.html#index_v"}]}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"}]}]}
--- doxygen/search/all_13.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_7ecommunicationplan',['~CommunicationPlan',['../classCabana_1_1CommunicationPlan.html#a84ecc7914419a76cc0f7ab49a5481104',1,'Cabana::CommunicationPlan']]]
-];
