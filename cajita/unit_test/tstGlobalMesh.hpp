d9f01eaaee6888a1241a67477a52250ca6b33e41
Stuart Slattery <slatterysr@ornl.gov>
2021-01-31 16:37:26 -0500

    add general dimensions to global mesh with 2d test

--- cajita/unit_test/tstGlobalMesh.hpp
+++ cajita/unit_test/tstGlobalMesh.hpp
@@ -24,7 +24,7 @@ namespace Test
 
 //---------------------------------------------------------------------------//
 // Test uniform mesh with cubic cells.
-void uniformTest1()
+void uniformTest3D1()
 {
     std::array<double, 3> low_corner = { -1.2, 0.1, 1.1 };
     std::array<double, 3> high_corner = { -0.3, 9.5, 1.3 };
@@ -53,7 +53,7 @@ void uniformTest1()
 
 //---------------------------------------------------------------------------//
 // Test uniform mesh with number of cells constructor.
-void uniformTest2()
+void uniformTest3D2()
 {
     std::array<double, 3> low_corner = { -1.2, 0.1, 1.1 };
     std::array<double, 3> high_corner = { -0.3, 9.5, 1.3 };
@@ -83,7 +83,7 @@ void uniformTest2()
 //---------------------------------------------------------------------------//
 // test uniform mesh with cells that can have a different size in each
 // dimension
-void uniformTest3()
+void uniformTest3D3()
 {
     std::array<double, 3> low_corner = { -1.2, 0.1, 1.1 };
     std::array<double, 3> high_corner = { -0.3, 9.5, 1.3 };
@@ -111,8 +111,96 @@ void uniformTest3()
 }
 
 //---------------------------------------------------------------------------//
-// test a non uniform mesh
-void nonUniformTest()
+// Test uniform mesh with cubic cells.
+void uniformTest2D1()
+{
+    std::array<double, 2> low_corner = { -1.2, 0.1 };
+    std::array<double, 2> high_corner = { -0.3, 9.5 };
+    double cell_size = 0.05;
+
+    auto global_mesh =
+        createUniformGlobalMesh( low_corner, high_corner, cell_size );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( low_corner[d], global_mesh->lowCorner( d ) );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( high_corner[d], global_mesh->highCorner( d ) );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( high_corner[d] - low_corner[d],
+                          global_mesh->extent( d ) );
+
+    std::array<int, 2> num_cell = { 18, 188 };
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_EQ( num_cell[d], global_mesh->globalNumCell( d ) );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( global_mesh->cellSize( d ), cell_size );
+}
+
+//---------------------------------------------------------------------------//
+// Test uniform mesh with number of cells constructor.
+void uniformTest2D2()
+{
+    std::array<double, 2> low_corner = { -1.2, 0.1 };
+    std::array<double, 2> high_corner = { -0.3, 9.5 };
+    std::array<int, 2> num_cell = { 18, 188 };
+
+    auto global_mesh =
+        createUniformGlobalMesh( low_corner, high_corner, num_cell );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( low_corner[d], global_mesh->lowCorner( d ) );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( high_corner[d], global_mesh->highCorner( d ) );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( high_corner[d] - low_corner[d],
+                          global_mesh->extent( d ) );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_EQ( num_cell[d], global_mesh->globalNumCell( d ) );
+
+    double cell_size = 0.05;
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( global_mesh->cellSize( d ), cell_size );
+}
+
+//---------------------------------------------------------------------------//
+// test uniform mesh with cells that can have a different size in each
+// dimension
+void uniformTest2D3()
+{
+    std::array<double, 2> low_corner = { -1.2, 0.1 };
+    std::array<double, 2> high_corner = { -0.3, 9.5 };
+    std::array<double, 2> cell_size = { 0.05, 0.05 };
+
+    auto global_mesh =
+        createUniformGlobalMesh( low_corner, high_corner, cell_size );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( low_corner[d], global_mesh->lowCorner( d ) );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( high_corner[d], global_mesh->highCorner( d ) );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( high_corner[d] - low_corner[d],
+                          global_mesh->extent( d ) );
+
+    std::array<int, 2> num_cell = { 18, 188 };
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_EQ( num_cell[d], global_mesh->globalNumCell( d ) );
+
+    for ( int d = 0; d < 2; ++d )
+        EXPECT_DOUBLE_EQ( global_mesh->cellSize( d ), cell_size[d] );
+}
+
+//---------------------------------------------------------------------------//
+// test a non uniform 3d mesh
+void nonUniformTest3d()
 {
     std::vector<float> i_edge = { -0.3, 0.4, 1.1 };
     std::vector<float> j_edge = { 3.3, 8.1, 9.5, 12.2 };
@@ -156,16 +244,59 @@ void nonUniformTest()
 }
 
 //---------------------------------------------------------------------------//
-// RUN TESTS
-//---------------------------------------------------------------------------//
-TEST( mesh, uniform_test )
+// test a non uniform 2d mesh
+void nonUniformTest2d()
 {
-    uniformTest1();
-    uniformTest2();
-    uniformTest3();
+    std::vector<float> i_edge = { -0.3, 0.4, 1.1 };
+    std::vector<float> j_edge = { 3.3, 8.1, 9.5, 12.2 };
+
+    auto global_mesh = createNonUniformGlobalMesh( i_edge, j_edge );
+
+    EXPECT_FLOAT_EQ( i_edge.front(), global_mesh->lowCorner( Dim::I ) );
+    EXPECT_FLOAT_EQ( j_edge.front(), global_mesh->lowCorner( Dim::J ) );
+
+    EXPECT_FLOAT_EQ( i_edge.back(), global_mesh->highCorner( Dim::I ) );
+    EXPECT_FLOAT_EQ( j_edge.back(), global_mesh->highCorner( Dim::J ) );
+
+    EXPECT_FLOAT_EQ( i_edge.back() - i_edge.front(),
+                     global_mesh->extent( Dim::I ) );
+    EXPECT_FLOAT_EQ( j_edge.back() - j_edge.front(),
+                     global_mesh->extent( Dim::J ) );
+
+    EXPECT_EQ( 2, global_mesh->globalNumCell( Dim::I ) );
+    EXPECT_EQ( 3, global_mesh->globalNumCell( Dim::J ) );
+
+    const auto& mesh_i = global_mesh->nonUniformEdge( Dim::I );
+    int ni = mesh_i.size();
+    for ( int i = 0; i < ni; ++i )
+        EXPECT_FLOAT_EQ( i_edge[i], mesh_i[i] );
+
+    const auto& mesh_j = global_mesh->nonUniformEdge( Dim::J );
+    int nj = mesh_j.size();
+    for ( int j = 0; j < nj; ++j )
+        EXPECT_FLOAT_EQ( j_edge[j], mesh_j[j] );
 }
 
-TEST( mesh, non_uniform_test ) { nonUniformTest(); }
+//---------------------------------------------------------------------------//
+// RUN TESTS
+//---------------------------------------------------------------------------//
+TEST( mesh, uniform_test_3d )
+{
+    uniformTest3D1();
+    uniformTest3D2();
+    uniformTest3D3();
+}
+
+TEST( mesh, uniform_test_2d )
+{
+    uniformTest2D1();
+    uniformTest2D2();
+    uniformTest2D3();
+}
+
+TEST( mesh, non_uniform_test_3d ) { nonUniformTest3d(); }
+
+TEST( mesh, non_uniform_test_2d ) { nonUniformTest2d(); }
 
 //---------------------------------------------------------------------------//
 
