5f6878a54f5dedc341c18e27f4a1d0f5a65bda70
YuxingQiu <yuxqiu@gmail.com>
2021-02-11 10:42:20 -0800

    organize partitioner

--- cajita/src/Cajita_SparseDimPartitioner.cpp
+++ cajita/src/Cajita_SparseDimPartitioner.cpp
@@ -4,39 +4,54 @@ namespace Cajita
 {
 //---------------------------------------------------------------------------//
 template <typename MemorySpace>
-SparseDimPartitioner::SparseDimPartitioner( MPI_Comm comm, 
-                                            const std::array<int, 3>& global_cells_per_dim )
-    : _workload_prefix_sum {Kokkos::View<int***, MemorySpace>( "rectangle_partition", 
-                                          global_cells_per_dim[0], 
-                                          global_cells_per_dim[1], 
-                                          global_cells_per_dim[2])},
-      _workload_per_cell {Kokkos::View<>( "workload", 
-                                         global_cells_per_dim[0], 
-                                         global_cells_per_dim[1], 
-                                         global_cells_per_dim[2])}
-{ 
-  // compute the available rank number (in each dimension)
-  ranksPerDimension(comm);
-  // init partitioner
-
+SparseDimPartitioner::SparseDimPartitioner(
+    MPI_Comm comm, float max_workload_coeff, int particle_num,
+    int num_step_rebalance, const std::array<int, 3>& global_cells_per_dim )
+    : _workload_threshold(
+          static_assert<int>( max_workload_coeff * particle_num ) )
+    , _num_step_rebalance( num_step_rebalance )
+    , _workload_prefix_sum{ Kokkos::View<int***, MemorySpace>(
+          "workload_prefix_sum",
+          global_cells_per_dim[0] >> cell_bits_per_tile_dim,
+          global_cells_per_dim[1] >> cell_bits_per_tile_dim,
+          global_cells_per_dim[2] >> cell_bits_per_tile_dim ) }
+    , _workload_per_cell{ Kokkos::View<int***, MemorySpace>(
+          "workload", global_cells_per_dim[0] >> cell_bits_per_tile_dim,
+          global_cells_per_dim[1] >> cell_bits_per_tile_dim,
+          global_cells_per_dim[2] >> cell_bits_per_tile_dim ) }
+{
+    // compute the available rank number (in each dimension)
+    ranksPerDimension( comm );
+    // init partitioner
 }
 
 template <typename MemorySpace>
-SparseDimPartitioner::SparseDimPartitioner( const std::array<int, 4>& ranks_per_dim, 
-                                            const std::array<int, 3>& global_cells_per_dim )
-    : _ranks_per_dim( ranks_per_dim ),
-      _workload_prefix_sum {Kokkos::View( "rectangle_partition", 
-                                          global_cells_per_dim[0], 
-                                          global_cells_per_dim[1], 
-                                          global_cells_per_dim[2])},
-      _workload_per_cell {Kokkos::View( "workload", 
-                                         global_cells_per_dim[0], 
-                                         global_cells_per_dim[1], 
-                                         global_cells_per_dim[2])}
-{ 
-  // init partitioner
+SparseDimPartitioner::SparseDimPartitioner(
+    float max_workload_coeff, int particle_num, int num_step_rebalance,
+    const std::array<int, 4>& ranks_per_dim,
+    const std::array<int, 3>& global_cells_per_dim )
+    : _workload_threshold(
+          static_assert<int>( max_workload_coeff * particle_num ) )
+    , _num_step_rebalance( num_step_rebalance )
+    , _ranks_per_dim( ranks_per_dim )
+    , _workload_prefix_sum{ Kokkos::View(
+          "workload_prefix_sum",
+          global_cells_per_dim[0] >> cell_bits_per_tile_dim,
+          global_cells_per_dim[1] >> cell_bits_per_tile_dim,
+          global_cells_per_dim[2] >> cell_bits_per_tile_dim ) }
+    , _workload_per_cell{ Kokkos::View(
+          "workload", global_cells_per_dim[0] >> cell_bits_per_tile_dim,
+          global_cells_per_dim[1] >> cell_bits_per_tile_dim,
+          global_cells_per_dim[2] >> cell_bits_per_tile_dim ) }
+{
+    // init partitioner
 }
 
+//---------------------------------------------------------------------------//
+std::array<int, 3> SparseDimPartitioner::ranksPerDimension() const
+{
+    return _ranks_per_dim;
+}
 //---------------------------------------------------------------------------//
 std::array<int, 3>
 SparseDimPartitioner::ranksPerDimension( MPI_Comm comm ) const
@@ -49,13 +64,12 @@ SparseDimPartitioner::ranksPerDimension( MPI_Comm comm ) const
 }
 
 //---------------------------------------------------------------------------//
-std::array<int,3>
-SparseDimPartitioner::ownedCellsPerDimension(
-    MPI_Comm cart_comm,
-    const std::array<int, 3>& ) const
+std::array<int, 3>
+SparseDimPartitioner::ownedCellsPerDimension( MPI_Comm cart_comm,
+                                              const std::array<int, 3>& ) const
 {
     // Get the Cartesian topology index of this rank.
-    std::array<int,3> cart_rank;
+    std::array<int, 3> cart_rank;
     int linear_rank;
     MPI_Comm_rank( cart_comm, &linear_rank );
     MPI_Cart_coords( cart_comm, linear_rank, 3, cart_rank.data() );
@@ -63,23 +77,65 @@ SparseDimPartitioner::ownedCellsPerDimension(
     // Get the cells per dimension and the remainder.
     std::array<int, 3> cells_per_dim;
     for ( int d = 0; d < 3; ++d )
-      cells_per_dim[d] = _rectangle_partition[d][cart_rank[d]+1] - _rectangle_partition[d][cart_rank[d]];
+        cells_per_dim[d] = _rectangle_partition[d][cart_rank[d] + 1] -
+                           _rectangle_partition[d][cart_rank[d]];
 
     return cells_per_dim;
 }
 
 //---------------------------------------------------------------------------//
 template <template ExecSpace, class ParticlePosViewType, template CellUnit>
-void SparseDimPartitioner::computeLocalWorkLoad(ParticlePosViewType &view, int particle_num, CellUnit dx);
+void SparseDimPartitioner::computeLocalWorkLoad( ParticlePosViewType& view,
+                                                 int particle_num, CellUnit dx )
 {
     Kokkos::parallel_for(
-      Kokkos::RangePolicy<ExecSpace>(0, particle_num),
-      KOKKOS_LAMBDA(cosnt int i){
-        int cell_i = static_cast<int>(view(i, 0) / dx - 0.5);
-        int cell_j = static_cast<int>(view(i, 1) / dx - 0.5);
-        int cell_k = static_cast<int>(view(i, 2) / dx - 0.5);
-        atomic_increment(&_workload_per_cell(cell_i, cell_j, cell_k)); 
-      });
+        Kokkos::RangePolicy<ExecSpace>( 0, particle_num ),
+        KOKKOS_LAMBDA( cosnt int i ) {
+            int cell_i = static_cast<int>( view( i, 0 ) / dx - 0.5 );
+            int cell_j = static_cast<int>( view( i, 1 ) / dx - 0.5 );
+            int cell_k = static_cast<int>( view( i, 2 ) / dx - 0.5 );
+            atomic_increment( &_workload_per_cell( cell_i, cell_j, cell_k ) );
+        } );
+}
+
+
+
+//---------------------------------------------------------------------------//
+void SparseDimPartitioner::computeFullPrefixSum()
+{
+    // all reduce the _workload_per_cell => _workloard_prefix_sum
+    // Can I directly work on the Kokkos::View here? Or do I need to make a
+    // host-mirror => Use Cuda aware MPI, no need for the host-mirror
+
+    // compute the prefix sum
+    // with interface from kokkos, explore how to deal with 3D
+}
+
+//---------------------------------------------------------------------------//
+void SparseDimPartitioner::optimizePartition()
+{
+    //
+}
+
+//---------------------------------------------------------------------------//
+void SparseDimPartitioner::greedyPartition()
+{
+    //
+}
+
+void SparseDimPartitioner::initialize_rec_partition(
+    std::vector<int>& rec_partition_i, std::vector<int>& rec_partition_j,
+    std::vector<int>& rec_partition_k )
+{
+    _rectangle_partition[0] = rec_partition_i;
+    _rectangle_partition[1] = rec_partition_j;
+    _rectangle_partition[2] = rec_partition_k;
+}
+
+template <class ParticlePosViewType>
+void SparseDimPartitioner::initialize_rec_partition(
+    ParticlePosViewType& pos_view )
+{
 }
 
 } // end namespace Cajita
--- cajita/src/Cajita_SparseDimPartitioner.hpp
+++ cajita/src/Cajita_SparseDimPartitioner.hpp
@@ -12,45 +12,85 @@
 namespace Cajita
 {
 //---------------------------------------------------------------------------//
-template<typename MemorySpace>
+template <typename MemorySpace, unsigned long long CellPerTileDim = 4>
 class SparseDimPartitioner : public Partitioner
 {
-  public: 
-    SparseDimPartitioner( MPI_Comm comm, const std::array<int, 3>& global_cells_per_dim );
-    SparseDimPartitioner( const std::array<int, 3>& ranks_per_dim, 
+  public:
+    //! Number of bits (per dimension) needed to index the cells inside a tile
+    static constexpr unsigned long long cell_bits_per_tile_dim =
+        bitCount( CellPerTileDim );
+    //! Number of cells inside each tile (per dimension), tile size reset to
+    //! power of 2
+    static constexpr unsigned long long cell_num_per_tile_dim =
+        1 << cell_bits_per_tile_dim;
+
+    SparseDimPartitioner( MPI_Comm comm, float max_workload_coeff,
+                          int particle_num, int num_step_rebalance,
+                          const std::array<int, 3>& global_cells_per_dim );
+    SparseDimPartitioner( float max_workload_coeff, int particle_num,
+                          int num_step_rebalance,
+                          const std::array<int, 3>& ranks_per_dim,
                           const std::array<int, 3>& global_cells_per_dim );
 
-    std::array<int, 3> ranksPerDimension(
-        MPI_Comm comm ) const override;
+    std::array<int, 3> ranksPerDimension( MPI_Comm comm ) const override;
+    std::array<int, 3> ranksPerDimension() const override;
 
-    std::array<int,3> ownedCellsPerDimension(
+    std::array<int, 3> ownedCellsPerDimension(
         MPI_Comm cart_comm,
         const std::array<int, 3>& global_cells_per_dim ) const override;
 
+    void initialize_rec_partition( std::vector<int>& rec_partition_i,
+                                   std::vector<int>& rec_partition_j,
+                                   std::vector<int>& rec_partition_k );
+
+    // sparse_map (where there are particles), Kokkos_array weight - tile =>
+    // workload cell > workload > partition > particle init template <class
+    // ParticlePosViewType> void initialize_rec_partition( ParticlePosViewType&
+    // pos_view );
+
+    // to compute the tileweight, assume tile_weight = 1 at the first place
+    template <template ExecSpace, class ParticlePosViewType, template CellUnit>
+    void SparseDimPartitioner::computeLocalWorkLoad( ParticlePosViewType& view,
+                                                     int particle_num,
+                                                     CellUnit dx );
     template <class ParticlePosViewType>
-    void computeLocalWorkload(ParticlePosViewType &pos_view);
+    void computeLocalWorkload( TileWeightViewType& tile_weight,
+                               SparseMapType& sparseMap );
+    // template <class ParticlePosViewType>
+    // void computeLocalWorkload( ParticlePosViewType& local_pos_view );
 
-    void computeFullPredixSum();
+    void computeFullPrefixSum();
 
-    void optimizePartition();
+    void optimiazePartition();
 
     void greedyPartition();
 
+    bool adaptive_load_balance();
+
   private:
-    //! represent the rectangle partition in each dimension 
-    //! with form [0, p_1, ..., p_n, cell_num], n = rank-num-in-current-dimension
-    //! partition in this dimension would be [0, p_1), [p_1, p_2) ... [p_n, cellNum]
-    std::array<std::vector<int>, 3> _rectangle_partition;
-    //! 3d prefix sum of the workload of each cell on current 
-    //! [TODO] remove to corresponding implementation?
-    Kokkos::View<int***, MemorySpace> _workload_prefix_sum;
-    Kokkos::View<int***, MemorySpace> _workload_per_cell;
-    //! ranks per dimension
-    std::array<int, 3> _ranks_per_dim;
-    //! workload_threshold_coeff
-    float _max_workload_coeff;
+    // ! workload_threshold_coeff
+    // float _max_workload_coeff;
     //! workload_threshold
     int _workload_threshold;
+    //! default check point for re-balance
+    int _num_step_rebalance;
+    //! represent the rectangle partition in each dimension
+    //! with form [0, p_1, ..., p_n, cell_num], n =
+    //! rank-num-in-current-dimension partition in this dimension would be [0,
+    //! p_1), [p_1, p_2) ... [p_n, cellNum]
+    std::array<std::vector<int>, 3> _rectangle_partition;
+    //! 3d prefix sum of the workload of each cell on current
+    //! [TODO] remove to corresponding implementation?
+    // current pre-set size: global_tile_per_dim * global_tile_per_dim*
+    // global_tile_per_dim
+    Kokkos::View<int***, MemorySpace> _workload_prefix_sum;
+    // current pre-set size: global_tile_per_dim * global_tile_per_dim*
+    // global_tile_per_dim
+    //! [TODO] rm, use _workload_perfix_sum only
+    Kokkos::View<int***, MemorySpace> _workload_per_cell;
+    // std::array<Kokkos::View<int***, MemorySpace>> _workload_buffer;
+    //! ranks per dimension
+    std::array<int, 3> _ranks_per_dim;
 };
 } // end namespace Cajita
 
