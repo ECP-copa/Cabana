29f7b2c14dd4fb50a332d9101c61f124f6455a40
Damien L-G <dalg24@gmail.com>
2021-02-18 14:32:41 -0500

    Let cabanacore be a header-only library

--- core/src/Cabana_Version.hpp
+++ core/src/Cabana_Version.hpp
@@ -19,9 +19,9 @@
 namespace Cabana
 {
 
-std::string version();
+std::string version() { return Cabana_VERSION_STRING; }
 
-std::string git_commit_hash();
+std::string git_commit_hash() { return Cabana_GIT_COMMIT_HASH; }
 
 } // end namespace Cabana
 
