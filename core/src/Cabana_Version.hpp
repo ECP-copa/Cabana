2e341b978a73f13e37c24bd9c632047f295b513e
Damien L-G <dalg24@gmail.com>
2021-02-18 15:09:01 -0500

    Fixup missing inline specifier for Cabana::{version, git_commit_hash}

--- core/src/Cabana_Version.hpp
+++ core/src/Cabana_Version.hpp
@@ -19,9 +19,9 @@
 namespace Cabana
 {
 
-std::string version() { return Cabana_VERSION_STRING; }
+inline std::string version() { return Cabana_VERSION_STRING; }
 
-std::string git_commit_hash() { return Cabana_GIT_COMMIT_HASH; }
+inline std::string git_commit_hash() { return Cabana_GIT_COMMIT_HASH; }
 
 } // end namespace Cabana
 
