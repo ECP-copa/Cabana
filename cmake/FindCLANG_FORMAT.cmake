7952632f7e9535f3535e74135327377718a35b8f
Christoph Junghans <christoph.junghans@gmail.com>
2021-07-26 11:54:49 -0600

    cmake: loosen clang-format regex

--- cmake/FindCLANG_FORMAT.cmake
+++ cmake/FindCLANG_FORMAT.cmake
@@ -41,11 +41,11 @@ if(CLANG_FORMAT_EXECUTABLE)
                   OUTPUT_VARIABLE clang_format_version
                   ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  if(clang_format_version MATCHES "^clang-format version .*")
+  if(clang_format_version MATCHES "^.*clang-format version .*")
     # clang_format_version sample: "clang-format version 3.9.1-4ubuntu3~16.04.1
     # (tags/RELEASE_391/rc2)"
     string(REGEX
-           REPLACE "clang-format version ([.0-9]+).*"
+           REPLACE "^.*clang-format version ([.0-9]+).*"
                    "\\1"
                    CLANG_FORMAT_VERSION
                    "${clang_format_version}")
