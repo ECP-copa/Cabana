0ccf4463c84b1ab1b0ff58f9636e189a73ea8670
Sam Reeve <6740307+streeve@users.noreply.github.com>
2022-04-01 11:13:18 -0400

    format ref solver

--- example/cajita_tutorial/11_structured_solver/structured_solver_example.cpp
+++ example/cajita_tutorial/11_structured_solver/structured_solver_example.cpp
@@ -32,17 +32,18 @@ void structuredSolverExample()
     std::array<bool, 3> is_dim_periodic = { false, false, false };
     std::array<double, 3> global_low_corner = { -1.0, -2.0, -1.0 };
     std::array<double, 3> global_high_corner = { 1.0, 1.0, 0.5 };
-    auto global_mesh = Cajita::createUniformGlobalMesh( global_low_corner,
-                                                global_high_corner, cell_size );
+    auto global_mesh = Cajita::createUniformGlobalMesh(
+        global_low_corner, global_high_corner, cell_size );
 
     // Create the global grid.
     Cajita::DimBlockPartitioner<3> partitioner;
     auto global_grid = Cajita::createGlobalGrid( MPI_COMM_WORLD, global_mesh,
-                                         is_dim_periodic, partitioner );
+                                                 is_dim_periodic, partitioner );
 
     // Create a local grid.
     auto local_mesh = createLocalGrid( global_grid, 1 );
-    auto owned_space = local_mesh->indexSpace( Cajita::Own(), Cajita::Cell(), Cajita::Local() );
+    auto owned_space = local_mesh->indexSpace( Cajita::Own(), Cajita::Cell(),
+                                               Cajita::Local() );
 
     // Create the RHS.
     auto vector_layout = createArrayLayout( local_mesh, 1, Cajita::Cell() );
@@ -64,7 +65,8 @@ void structuredSolverExample()
     solver->setMatrixStencil( stencil );
 
     // Create the matrix entries. The stencil is defined over cells.
-    auto matrix_entry_layout = createArrayLayout( local_mesh, 7, Cajita::Cell() );
+    auto matrix_entry_layout =
+        createArrayLayout( local_mesh, 7, Cajita::Cell() );
     auto matrix_entries = Cajita::createArray<double, MemorySpace>(
         "matrix_entries", matrix_entry_layout );
     auto entry_view = matrix_entries->view();
@@ -96,8 +98,9 @@ void structuredSolverExample()
     std::string precond_type = "Diagonal";
     if ( "none" != precond_type )
     {
-        auto preconditioner = Cajita::createHypreStructuredSolver<double, MemorySpace>(
-            precond_type, *vector_layout, true );
+        auto preconditioner =
+            Cajita::createHypreStructuredSolver<double, MemorySpace>(
+                precond_type, *vector_layout, true );
         solver->setPreconditioner( preconditioner );
     }
 
@@ -108,25 +111,34 @@ void structuredSolverExample()
     solver->solve( *rhs, *lhs );
 
     // Create a solver reference for comparison.
-    auto lhs_ref = Cajita::createArray<double, MemorySpace>( "lhs_ref", vector_layout );
+    auto lhs_ref =
+        Cajita::createArray<double, MemorySpace>( "lhs_ref", vector_layout );
     Cajita::ArrayOp::assign( *lhs_ref, 0.0, Cajita::Own() );
 
     auto ref_solver =
-        Cajita::createReferenceConjugateGradient<double, MemorySpace>( *vector_layout );
+        Cajita::createReferenceConjugateGradient<double, MemorySpace>(
+            *vector_layout );
     ref_solver->setMatrixStencil( stencil );
     const auto& ref_entries = ref_solver->getMatrixValues();
     auto matrix_view = ref_entries.view();
-    auto global_space = local_mesh->indexSpace( Cajita::Own(), Cajita::Cell(), Cajita::Global() );
-    int ncell_i = global_grid->globalNumEntity( Cajita::Cell(), Cajita::Dim::I );
-    int ncell_j = global_grid->globalNumEntity( Cajita::Cell(), Cajita::Dim::J );
-    int ncell_k = global_grid->globalNumEntity( Cajita::Cell(), Cajita::Dim::K );
+    auto global_space = local_mesh->indexSpace( Cajita::Own(), Cajita::Cell(),
+                                                Cajita::Global() );
+    int ncell_i =
+        global_grid->globalNumEntity( Cajita::Cell(), Cajita::Dim::I );
+    int ncell_j =
+        global_grid->globalNumEntity( Cajita::Cell(), Cajita::Dim::J );
+    int ncell_k =
+        global_grid->globalNumEntity( Cajita::Cell(), Cajita::Dim::K );
     Kokkos::parallel_for(
         "fill_ref_entries",
         createExecutionPolicy( owned_space, ExecutionSpace() ),
         KOKKOS_LAMBDA( const int i, const int j, const int k ) {
-            int gi = i + global_space.min( Cajita::Dim::I ) - owned_space.min( Cajita::Dim::I );
-            int gj = j + global_space.min( Cajita::Dim::J ) - owned_space.min( Cajita::Dim::J );
-            int gk = k + global_space.min( Cajita::Dim::K ) - owned_space.min( Cajita::Dim::K );
+            int gi = i + global_space.min( Cajita::Dim::I ) -
+                     owned_space.min( Cajita::Dim::I );
+            int gj = j + global_space.min( Cajita::Dim::J ) -
+                     owned_space.min( Cajita::Dim::J );
+            int gk = k + global_space.min( Cajita::Dim::K ) -
+                     owned_space.min( Cajita::Dim::K );
             matrix_view( i, j, k, 0 ) = 6.0;
             matrix_view( i, j, k, 1 ) = ( gi - 1 >= 0 ) ? -1.0 : 0.0;
             matrix_view( i, j, k, 2 ) = ( gi + 1 < ncell_i ) ? -1.0 : 0.0;
@@ -155,20 +167,20 @@ void structuredSolverExample()
     // Check the results.
     auto lhs_host =
         Kokkos::create_mirror_view_and_copy( MemorySpace(), lhs->view() );
-    auto lhs_ref_host = Kokkos::create_mirror_view_and_copy(
-        MemorySpace(), lhs_ref->view() );
-    for ( int i = owned_space.min( Cajita::Dim::I ); i < owned_space.max( Cajita::Dim::I );
-          ++i )
-        for ( int j = owned_space.min( Cajita::Dim::J ); j < owned_space.max( Cajita::Dim::J );
-              ++j )
+    auto lhs_ref_host =
+        Kokkos::create_mirror_view_and_copy( MemorySpace(), lhs_ref->view() );
+    for ( int i = owned_space.min( Cajita::Dim::I );
+          i < owned_space.max( Cajita::Dim::I ); ++i )
+        for ( int j = owned_space.min( Cajita::Dim::J );
+              j < owned_space.max( Cajita::Dim::J ); ++j )
             for ( int k = owned_space.min( Cajita::Dim::K );
                   k < owned_space.max( Cajita::Dim::K ); ++k )
-                //EXPECT_FLOAT_EQ( lhs_host( i, j, k, 0 ),
+                // EXPECT_FLOAT_EQ( lhs_host( i, j, k, 0 ),
                 //                 lhs_ref_host( i, j, k, 0 ) );
 
-    // Setup the problem again. We would need to do this if we changed the
-    // matrix entries.
-    solver->setup();
+                // Setup the problem again. We would need to do this if we
+                // changed the matrix entries.
+                solver->setup();
 
     // Solve the problem again
     Cajita::ArrayOp::assign( *rhs, 2.0, Cajita::Own() );
@@ -180,13 +192,15 @@ void structuredSolverExample()
     ref_solver->solve( *rhs, *lhs_ref );
 
     //// Check the results again
-    //lhs_host =
+    // lhs_host =
     //    Kokkos::create_mirror_view_and_copy( MemorySpace(), lhs->view() );
-    //lhs_ref_host = Kokkos::create_mirror_view_and_copy( MemorySpace(),
+    // lhs_ref_host = Kokkos::create_mirror_view_and_copy( MemorySpace(),
     //                                                    lhs_ref->view() );
-    //for ( int i = owned_space.min( Cajita::Dim::I ); i < owned_space.max( Cajita::Dim::I );
+    // for ( int i = owned_space.min( Cajita::Dim::I ); i < owned_space.max(
+    // Cajita::Dim::I );
     //      ++i )
-    //    for ( int j = owned_space.min( Cajita::Dim::J ); j < owned_space.max( Cajita::Dim::J );
+    //    for ( int j = owned_space.min( Cajita::Dim::J ); j < owned_space.max(
+    //    Cajita::Dim::J );
     //          ++j )
     //        for ( int k = owned_space.min( Cajita::Dim::K );
     //              k < owned_space.max( Cajita::Dim::K ); ++k )
