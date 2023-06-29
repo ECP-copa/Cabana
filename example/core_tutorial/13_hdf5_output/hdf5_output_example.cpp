94e431549daf5f73717884fc33cb9c49ca11149e
Sam Reeve <6740307+streeve@users.noreply.github.com>
2023-06-29 13:35:44 -0400

    Update HDF5 example

--- example/core_tutorial/13_hdf5_output/hdf5_output_example.cpp
+++ example/core_tutorial/13_hdf5_output/hdf5_output_example.cpp
@@ -19,26 +19,33 @@
 void hdf5_output()
 {
     /*
-      In the current example, we will illustrate the process of storing a list
-      of particles with properties, such as position, velocity, mass, radius,
-      etc., in an hdf5 file format. This example is built up on previous
-      examples such as, 04_aosoa example.
+      HDF5 is a parallel file format for large datasets. In this example, we
+      will illustrate the process of storing a list of particles with
+      properties, such as position, velocity, mass, radius, etc., in an HDF5
+      file format.
     */
 
-    std::cout << "Cabana HDF5 output example\n" << std::endl;
+    /*
+       Get parameters from the communicator. We will use MPI_COMM_WORLD for
+       this example but any MPI communicator may be used.
+    */
+    int comm_rank = -1;
+    MPI_Comm_rank( MPI_COMM_WORLD, &comm_rank );
+
+    if ( comm_rank == 0 )
+        std::cout << "Cabana HDF5 output example\n" << std::endl;
 
     /*
-      Start by declaring the types in our tuples will store. The first element
+      Start by declaring the types the particles will store. The first element
       will represent the coordinates, the second will be the particle's ID, the
-      third will represent the velocity, and the fourth will represent the
-      radius of the particle.
+      third velocity, and the fourth the radius of the particle.
     */
     using DataTypes = Cabana::MemberTypes<double[3], int, double[3], double>;
 
     /*
       Next declare the data layout of the AoSoA. We use the host space here
       for the purposes of this example but all memory spaces, vector lengths,
-      and member type configurations are compatible with neighbor lists.
+      and member type configurations are compatible.
     */
     const int VectorLength = 8;
     using MemorySpace = Kokkos::HostSpace;
@@ -48,121 +55,102 @@ void hdf5_output()
     /*
       Create the AoSoA.
     */
-    int num_tuple = 9;
-    Cabana::AoSoA<DataTypes, DeviceType, VectorLength> aosoa( "A", num_tuple );
-
-    /*
-      Define the parameters of the Cartesian grid over which we will build the
-      particles. This is a simple 3x3x3 uniform grid on [0,3] in each
-      direction. Each grid cell has a size of 1 in each dimension.
-    */
-    double grid_min[3] = { 0.0, 0.0, 0.0 };
-    double grid_max[3] = { 3.0, 3.0, 3.0 };
-    double grid_delta[3] = { 1.0, 1.0, 1.0 };
-
-    /*
-      Create the particle ids.
-    */
+    int num_particles = 9;
+    Cabana::AoSoA<DataTypes, DeviceType, VectorLength> aosoa( "A",
+                                                              num_particles );
 
     /*
       Get the particle ids, coordinates, velocity, radius
     */
-    auto ids = Cabana::slice<1>( aosoa, "ids" );
     auto positions = Cabana::slice<0>( aosoa, "positions" );
+    auto ids = Cabana::slice<1>( aosoa, "ids" );
     auto velocity = Cabana::slice<2>( aosoa, "velocity" );
     auto radius = Cabana::slice<3>( aosoa, "radius" );
 
     // initialize the particle properties
     for ( std::size_t i = 0; i < aosoa.size(); ++i )
     {
-
         // set ids of the particles
         ids( i ) = i;
 
         // set positions of the particles
-        positions( i, 0 ) = grid_min[0] + grid_delta[0] * ( 2. * i );
+        positions( i, 0 ) = 2. * i;
         positions( i, 1 ) = 0.;
         positions( i, 2 ) = 0.;
 
         // set the velocity of each particle
         velocity( i, 0 ) = 1.;
-        velocity( i, 1 ) = 1.;
+        velocity( i, 1 ) = 0.;
         velocity( i, 2 ) = 0.;
 
         // set the radius of each particle
         radius( i ) = 0.1;
     }
 
+    // A configuration object is necessary for tuning HDF5 options.
+    Cabana::Experimental::HDF5ParticleOutput::HDF5Config h5_config;
+
+    // For example, MPI I/O is independent by default, but collective operations
+    // can be enabled by setting:
+    h5_config.collective = true;
+
     /*
-      We will evolve the system for a total time of 1000 timesteps. We will
-      output the properties at an interval of every 100 steps.
+      We will evolve the system for a total time of 100 timesteps and
+      output the properties at an interval of every 10 steps.
     */
-    auto dt = 1e-2;       // timestep
-    auto tf = 1000. * dt; // total time
-    auto t = 0.;          // time
-    int nsteps = tf / dt; // total steps
-    int pfreq = 100;      // print frequency
+    auto dt = 1e-2;
+    auto final_time = 100. * dt;
+    auto time = 0.;
+    int steps = final_time / dt;
+    int print_freq = 10;
+
     // Main timestep loop
-    for ( int step = 0; step < nsteps; step++ )
+    for ( int step = 0; step < steps; step++ )
     {
-        /*
-          move positions
-        */
+        // Update positions
         Kokkos::RangePolicy<ExecutionSpace> policy( 0, aosoa.size() );
         Kokkos::parallel_for(
             "move_particles", policy, KOKKOS_LAMBDA( const int i ) {
                 positions( i, 0 ) += velocity( i, 0 ) * dt;
-                positions( i, 1 ) += velocity( i, 0 ) * dt;
-                positions( i, 2 ) += velocity( i, 0 ) * dt;
+                positions( i, 1 ) += velocity( i, 1 ) * dt;
+                positions( i, 2 ) += velocity( i, 2 ) * dt;
             } );
         Kokkos::fence();
 
-        // This is for setting HDF5 options
-        Cabana::Experimental::HDF5ParticleOutput::HDF5Config h5_config;
-        if ( step % pfreq == 0 )
+        if ( step % print_freq == 0 )
         {
-
             /*
-              Inorder to write the particle properties to an hdf5 files, we use
+              Now we write all particle properties to HDF5 files. The arguments
+              are as follows:
 
-              Cabana::Experimental::HDF5ParticleOutput::writeTimeStep function.
-
-              First seven arguments of `writeTimeStep` are mandotory arguments
-              and rest are optional fields. The seven arguments are as follows
-
-
-              1. config file
-              2. name of the file
-              3. MPI communication
-              4. time step value (int)
-              5. time of the simulation (double)
-              6. number of particles (int)
-              7. positions (double[3])
-              8. Optional fields
-              9. Optional fields
-              10. ...
-
-              Please look at the source for more details
-              https://github.com/ECP-copa/Cabana/blob/master/core/src/Cabana_HDF5ParticleOutput.hpp#L496
+              1. HDF5 configuration: specify tuning details
+              2. output file prefix
+              3. MPI communicator
+              4. Current time step
+              5. Current simulation time
+              6. Number of particles to output (it is often desirable to ignore
+                 ghosted particles)
+              7. Position slice
+              8. Variadic list of particle fields
+              9. ...
             */
             Cabana::Experimental::HDF5ParticleOutput::writeTimeStep(
-                h5_config,
-                "particles",    // name of the file
-                MPI_COMM_WORLD, //
-                step,           // step
-                t,              // time of the simulation
-                num_tuple,      // no of points
-                positions,      // positions of the points (double[3])
-                ids,            // Optional fields
-                velocity,       // Optional fields
-                radius          // Optional fields
-                // ...  // Optional fields
-                // ...  // Optional fields
-            );
+                h5_config, "particles", MPI_COMM_WORLD, step, time,
+                num_particles, positions, ids, velocity, radius );
+
+            if ( comm_rank == 0 )
+                std::cout << "Output for step " << step << "/" << steps
+                          << std::endl;
         }
 
-        t += dt;
+        time += dt;
     }
+
+    /*
+      The created HDF5 files with XMF metadata can be read by many visualization
+      programs. This example can be run with any number of MPI ranks, for
+      parallel particle output.
+    */
 }
 
 int main( int argc, char* argv[] )
