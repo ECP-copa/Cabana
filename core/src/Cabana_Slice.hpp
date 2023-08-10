a6970aebfec2cbbd7db3113960d008f263d88d76
Sam Reeve <6740307+streeve@users.noreply.github.com>
2023-08-10 14:37:54 -0400

    Add View/slice size free functions

--- core/src/Cabana_Slice.hpp
+++ core/src/Cabana_Slice.hpp
@@ -983,6 +983,23 @@ void copyViewToSlice( ViewType& view, const SliceType& slice,
 
 //---------------------------------------------------------------------------//
 
+//! Check slice size (differs from Kokkos View).
+template <class SliceType>
+auto size( SliceType slice,
+           typename std::enable_if<is_slice<SliceType>::value, int>::type* = 0 )
+{
+    return slice.size();
+}
+
+//! Check View size (differs from Slice).
+template <class ViewType>
+auto size(
+    ViewType view,
+    typename std::enable_if<Kokkos::is_view<ViewType>::value, int>::type* = 0 )
+{
+    return view.extent( 0 );
+}
+
 } // end namespace Cabana
 
 //---------------------------------------------------------------------------//
