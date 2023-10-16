2ecf40db9ab2bfe494bd6ad3b028d9f038c0a6a6
Sam Reeve <6740307+streeve@users.noreply.github.com>
2023-10-16 16:22:01 -0400

    Benchmark plot option for comparing multiple files

--- benchmark/plot/Cabana_PlotBenchmark.py
+++ benchmark/plot/Cabana_PlotBenchmark.py
@@ -45,8 +45,30 @@ def plotCompareHostDevice(ax, data, compare="host"):
                     num_2 = len(result2.times)
                     max = num_1 if num_1 < num_2 else num_2
 
-                    sizes = scaleSizes(result.sizes, data.grid)
-                    speedup = result2.times / result.times
+                    sizes = scaleSizes(result.sizes[:max], data.grid)
+                    speedup = result2.times[:max] / result.times[:max]
+                    plotResults(ax, sizes, speedup, backend, color_dict[cat])
+    return True
+
+# Compare host and device results from a list of files.
+def plotCompare(ax, data, compare=["buffer"]):
+    color_dict = getColors(data)
+    for backend in data.getAllBackends():
+        if "host" in backend: continue
+        for cat in data.getAllCategories():
+            for type in data.getAllTypes():
+                for param in data.getAllParams():
+                    desc = ManualDataDescription(backend, type, cat, compare+param)
+                    result = AllSizesSingleResult(data, desc)
+                    desc2 = ManualDataDescription(backend, type, cat, param)
+                    result2 = AllSizesSingleResult(data, desc2)
+
+                    num_1 = len(result.times)
+                    num_2 = len(result2.times)
+                    max = num_1 if num_1 < num_2 else num_2
+
+                    sizes = scaleSizes(result.sizes[:max], data.grid)
+                    speedup = result2.times[:max] / result.times[:max]
                     plotResults(ax, sizes, speedup, backend, color_dict[cat])
     return True
 
@@ -92,6 +114,7 @@ if __name__ == "__main__":
 
     speedup = plotAll(ax1, data)
     #speedup = plotCompareHostDevice(ax1, data)
+    #speedup = plotCompare(ax1, data)
 
     #data, data_f2 = getSeparateData(filelist)
     #speedup = plotCompareFiles(ax1, data, data_f2)
