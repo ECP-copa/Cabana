551b8784cf6ebc0f8b6c2c646dab597a3767ee4a
Automatic Deployment (GitHub Action) <noreply@lanl.gov>
2023-10-16 21:31:53 +0000

    Documentation Update

--- doxygen/search/classes_11.js
+++ doxygen/search/classes_11.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['uniformdimpartitioner_0',['UniformDimPartitioner',['../classCajita_1_1UniformDimPartitioner.html',1,'Cajita']]],
-  ['uniformmesh_1',['UniformMesh',['../structCabana_1_1Grid_1_1UniformMesh.html',1,'Cabana::Grid']]]
+  ['uniformmesh_0',['UniformMesh',['../structCabana_1_1Grid_1_1UniformMesh.html',1,'Cabana::Grid']]]
 ];
--- doxygen/search/classes_a.js
+++ doxygen/search/classes_a.js
@@ -1,17 +1,16 @@
 var searchData=
 [
   ['manualblockpartitioner_0',['ManualBlockPartitioner',['../classCabana_1_1Grid_1_1ManualBlockPartitioner.html',1,'Cabana::Grid']]],
-  ['manualpartitioner_1',['ManualPartitioner',['../classCajita_1_1ManualPartitioner.html',1,'Cajita']]],
-  ['matrix_2',['Matrix',['../structCabana_1_1Field_1_1Matrix.html',1,'Cabana::Field']]],
-  ['max_3',['Max',['../structCabana_1_1Grid_1_1ScatterReduce_1_1Max.html',1,'Cabana::Grid::ScatterReduce']]],
-  ['membertypeatindex_4',['MemberTypeAtIndex',['../structCabana_1_1MemberTypeAtIndex.html',1,'Cabana']]],
-  ['membertypeatindex_3c_20m_2c_20membertypes_3c_20types_2e_2e_2e_20_3e_20_3e_5',['MemberTypeAtIndex&lt; M, MemberTypes&lt; Types... &gt; &gt;',['../structCabana_1_1MemberTypeAtIndex_3_01M_00_01MemberTypes_3_01Types_8_8_8_01_4_01_4.html',1,'Cabana']]],
-  ['membertypes_6',['MemberTypes',['../structCabana_1_1MemberTypes.html',1,'Cabana']]],
-  ['min_7',['Min',['../structCabana_1_1Grid_1_1ScatterReduce_1_1Min.html',1,'Cabana::Grid::ScatterReduce']]],
-  ['mpitraits_8',['MpiTraits',['../structCabana_1_1Grid_1_1MpiTraits.html',1,'Cabana::Grid']]],
-  ['mpitraits_3c_20char_20_3e_9',['MpiTraits&lt; char &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01char_01_4.html',1,'Cabana::Grid']]],
-  ['mpitraits_3c_20double_20_3e_10',['MpiTraits&lt; double &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01double_01_4.html',1,'Cabana::Grid']]],
-  ['mpitraits_3c_20float_20_3e_11',['MpiTraits&lt; float &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01float_01_4.html',1,'Cabana::Grid']]],
-  ['mpitraits_3c_20int_20_3e_12',['MpiTraits&lt; int &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01int_01_4.html',1,'Cabana::Grid']]],
-  ['mpitraits_3c_20long_20_3e_13',['MpiTraits&lt; long &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01long_01_4.html',1,'Cabana::Grid']]]
+  ['matrix_1',['Matrix',['../structCabana_1_1Field_1_1Matrix.html',1,'Cabana::Field']]],
+  ['max_2',['Max',['../structCabana_1_1Grid_1_1ScatterReduce_1_1Max.html',1,'Cabana::Grid::ScatterReduce']]],
+  ['membertypeatindex_3',['MemberTypeAtIndex',['../structCabana_1_1MemberTypeAtIndex.html',1,'Cabana']]],
+  ['membertypeatindex_3c_20m_2c_20membertypes_3c_20types_2e_2e_2e_20_3e_20_3e_4',['MemberTypeAtIndex&lt; M, MemberTypes&lt; Types... &gt; &gt;',['../structCabana_1_1MemberTypeAtIndex_3_01M_00_01MemberTypes_3_01Types_8_8_8_01_4_01_4.html',1,'Cabana']]],
+  ['membertypes_5',['MemberTypes',['../structCabana_1_1MemberTypes.html',1,'Cabana']]],
+  ['min_6',['Min',['../structCabana_1_1Grid_1_1ScatterReduce_1_1Min.html',1,'Cabana::Grid::ScatterReduce']]],
+  ['mpitraits_7',['MpiTraits',['../structCabana_1_1Grid_1_1MpiTraits.html',1,'Cabana::Grid']]],
+  ['mpitraits_3c_20char_20_3e_8',['MpiTraits&lt; char &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01char_01_4.html',1,'Cabana::Grid']]],
+  ['mpitraits_3c_20double_20_3e_9',['MpiTraits&lt; double &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01double_01_4.html',1,'Cabana::Grid']]],
+  ['mpitraits_3c_20float_20_3e_10',['MpiTraits&lt; float &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01float_01_4.html',1,'Cabana::Grid']]],
+  ['mpitraits_3c_20int_20_3e_11',['MpiTraits&lt; int &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01int_01_4.html',1,'Cabana::Grid']]],
+  ['mpitraits_3c_20long_20_3e_12',['MpiTraits&lt; long &gt;',['../structCabana_1_1Grid_1_1MpiTraits_3_01long_01_4.html',1,'Cabana::Grid']]]
 ];
