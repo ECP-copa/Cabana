0cb68a718fa7980312d111c8047a48003b275694
Sam Reeve <6740307+streeve@users.noreply.github.com>
2023-11-16 09:50:45 -0500

    fixup: simplify deprecated sparse Cajita wrappers

--- grid/src/Cabana_Grid_SparseArray.hpp
+++ grid/src/Cabana_Grid_SparseArray.hpp
@@ -721,38 +721,32 @@ using SparseArrayLayout CAJITA_DEPRECATED =
     Cabana::Grid::Experimental::SparseArrayLayout<DataTypes, EntityType,
                                                   MeshType, SparseMapType>;
 
-template <class DataTypes, class EntityType, class MeshType,
-          class SparseMapType>
-CAJITA_DEPRECATED auto createSparseArrayLayout(
-    const std::string label,
-    SparseArrayLayout<DataTypes, EntityType, MeshType, SparseMapType>& layout )
+template <class... Args>
+CAJITA_DEPRECATED auto createSparseArrayLayout( Args&&... args )
 {
-    return Cabana::Grid::Experimental::createSparseArrayLayout( label, layout );
+    return Cabana::Grid::Experimental::createSparseArrayLayout(
+        std::forward<Args>( args )... );
 }
 
 template <class T>
 using is_sparse_array_layout CAJITA_DEPRECATED =
     Cabana::Grid::Experimental::is_sparse_array_layout<T>;
 
-template <class DataTypes, class DeviceType, class EntityType, class MeshType,
+template <class DataTypes, class MemorySpace, class EntityType, class MeshType,
           class SparseMapType>
 using SparseArray CAJITA_DEPRECATED =
-    Cabana::Grid::Experimental::SparseArray<DataTypes, DeviceType, EntityType,
+    Cabana::Grid::Experimental::SparseArray<DataTypes, MemorySpace, EntityType,
                                             MeshType, SparseMapType>;
 
 template <class T>
 using is_sparse_array CAJITA_DEPRECATED =
     Cabana::Grid::Experimental::is_sparse_array<T>;
 
-template <class DeviceType, class DataTypes, class EntityType, class MeshType,
-          class SparseMapType>
-CAJITA_DEPRECATED auto
-createSparseArray( const std::string label,
-                   SparseArray<DataTypes, DeviceType, EntityType, MeshType,
-                               SparseMapType>& layout )
+template <class MemorySpace, class... Args>
+CAJITA_DEPRECATED auto createSparseArray( Args&&... args )
 {
-    return Cabana::Grid::Experimental::createSparseArray<DeviceType>( label,
-                                                                      layout );
+    return Cabana::Grid::Experimental::createSparseArray<MemorySpace>(
+        std::forward<Args>( args )... );
 }
 //! \endcond
 } // namespace Experimental
