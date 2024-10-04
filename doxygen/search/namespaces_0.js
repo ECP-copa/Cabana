badc1c64d4ce678d0069cdde55956d0184d1482c
Automatic Deployment (GitHub Action) <noreply@lanl.gov>
2024-10-04 16:56:18 +0000

    Documentation Update

--- doxygen/search/namespaces_0.js
+++ doxygen/search/namespaces_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['cabana_0',['Cabana',['../namespaceCabana.html',1,'']]],
-  ['cajita_1',['Cajita',['../namespaceCajita.html',1,'']]]
+  ['cabana_0',['Cabana',['../namespaceCabana.html',1,'']]]
 ];
