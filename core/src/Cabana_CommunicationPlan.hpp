1b9e0f548d526fb77469b95fee2d5bc958d231e5
Jason Stewart <jason00stewart@gmail.com>
2025-06-17 10:36:53 -0600

    ran clang format

--- core/src/Cabana_CommunicationPlan.hpp
+++ core/src/Cabana_CommunicationPlan.hpp
@@ -661,7 +661,9 @@ class CommunicationPlan
         const int ec =
             MPI_Waitall( requests.size(), requests.data(), status.data() );
         if ( MPI_SUCCESS != ec )
-            throw std::logic_error( "Cabana::CommunicationPlan::createFromExportsAndTopology: Failed MPI Communication" );
+            throw std::logic_error(
+                "Cabana::CommunicationPlan::createFromExportsAndTopology: "
+                "Failed MPI Communication" );
 
         // Get the total number of imports/exports.
         _total_num_export = std::accumulate(
@@ -843,7 +845,9 @@ class CommunicationPlan
         const int ec =
             MPI_Waitall( requests.size(), requests.data(), status.data() );
         if ( MPI_SUCCESS != ec )
-            throw std::logic_error( "Cabana::CommunicationPlan::createFromExportsOnly: Failed MPI Communication" );
+            throw std::logic_error(
+                "Cabana::CommunicationPlan::createFromExportsOnly: Failed MPI "
+                "Communication" );
 
         // Compute the total number of imports.
         _total_num_import =
@@ -988,7 +992,9 @@ class CommunicationPlan
 
         if ( !use_iota &&
              ( element_export_ids.size() != element_export_ranks.size() ) )
-            throw std::runtime_error( "Cabana::CommunicationPlan::createSteering: Export ids and ranks different sizes!" );
+            throw std::runtime_error(
+                "Cabana::CommunicationPlan::createSteering: Export ids and "
+                "ranks different sizes!" );
 
         // Get the size of this communicator.
         int comm_size = -1;
@@ -1266,7 +1272,8 @@ class CommunicationData
                       const double overallocation )
     {
         if ( overallocation < 1.0 )
-            throw std::runtime_error( "Cabana::CommunicationPlan::reserveImpl: Cannot allocate buffers with less space "
+            throw std::runtime_error( "Cabana::CommunicationPlan::reserveImpl: "
+                                      "Cannot allocate buffers with less space "
                                       "than data to communicate!" );
         _overallocation = overallocation;
 
--- grid/src/Cabana_Grid_HypreStructuredSolver.hpp
+++ grid/src/Cabana_Grid_HypreStructuredSolver.hpp
@@ -232,24 +232,29 @@ class HypreStructuredSolver
         static_assert( is_array<Array_t>::value, "Must use an array" );
         static_assert(
             std::is_same<typename Array_t::entity_type, entity_type>::value,
-            "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Array entity type mush match solver entity type" );
+            "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Array entity "
+            "type mush match solver entity type" );
         static_assert(
             std::is_same<typename Array_t::memory_space, MemorySpace>::value,
-            "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Array memory space and solver memory space are different." );
+            "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Array memory "
+            "space and solver memory space are different." );
 
         static_assert(
             std::is_same<typename Array_t::value_type, value_type>::value,
-            "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Array value type and solver value type are different." );
+            "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Array value "
+            "type and solver value type are different." );
 
         // This function is only valid for non-preconditioners.
         if ( _is_preconditioner )
             throw std::logic_error(
-                "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Cannot call setMatrixValues() on preconditioners" );
+                "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Cannot "
+                "call setMatrixValues() on preconditioners" );
 
         if ( values.layout()->dofsPerEntity() !=
              static_cast<int>( _stencil_size ) )
             throw std::runtime_error(
-                "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Number of matrix values does not match stencil size" );
+                "Cabana::Grid::HypreStructuredSolver:setMatrixValues: Number "
+                "of matrix values does not match stencil size" );
 
         // Spatial dimension.
         const std::size_t num_space_dim = Array_t::num_space_dim;
@@ -328,11 +333,13 @@ class HypreStructuredSolver
         // This function is only valid for non-preconditioners.
         if ( _is_preconditioner )
             throw std::logic_error(
-                "Cabana::Grid::HypreStructuredSolver:setPreconditioner: Cannot call setPreconditioner() on a preconditioner" );
+                "Cabana::Grid::HypreStructuredSolver:setPreconditioner: Cannot "
+                "call setPreconditioner() on a preconditioner" );
 
         // Only a preconditioner can be used as a preconditioner.
         if ( !preconditioner->isPreconditioner() )
-            throw std::logic_error( "Cabana::Grid::HypreStructuredSolver:setPreconditioner: Not a preconditioner" );
+            throw std::logic_error( "Cabana::Grid::HypreStructuredSolver:"
+                                    "setPreconditioner: Not a preconditioner" );
 
         _preconditioner = preconditioner;
         this->setPreconditionerImpl( *_preconditioner );
@@ -343,7 +350,8 @@ class HypreStructuredSolver
     {
         // This function is only valid for non-preconditioners.
         if ( _is_preconditioner )
-            throw std::logic_error( "Cabana::Grid::HypreStructuredSolver:setup: Cannot call setup() on preconditioners" );
+            throw std::logic_error( "Cabana::Grid::HypreStructuredSolver:setup:"
+                                    " Cannot call setup() on preconditioners" );
 
         // FIXME: appears to be a memory issue in the call to this function
         this->setupImpl();
@@ -360,26 +368,34 @@ class HypreStructuredSolver
         Kokkos::Profiling::ScopedRegion region(
             "Cabana::Grid::HypreStructuredSolver::solve" );
 
-        static_assert( is_array<Array_t>::value, "Cabana::Grid::HypreStructuredSolver::solve: Must use an array" );
+        static_assert(
+            is_array<Array_t>::value,
+            "Cabana::Grid::HypreStructuredSolver::solve: Must use an array" );
         static_assert(
             std::is_same<typename Array_t::entity_type, entity_type>::value,
-            "Cabana::Grid::HypreStructuredSolver::solve: Array entity type mush match solver entity type" );
+            "Cabana::Grid::HypreStructuredSolver::solve: Array entity type "
+            "mush match solver entity type" );
         static_assert(
             std::is_same<typename Array_t::memory_space, MemorySpace>::value,
-            "Cabana::Grid::HypreStructuredSolver::solve: Array memory space and solver memory space are different." );
+            "Cabana::Grid::HypreStructuredSolver::solve: Array memory space "
+            "and solver memory space are different." );
 
         static_assert(
             std::is_same<typename Array_t::value_type, value_type>::value,
-            "Cabana::Grid::HypreStructuredSolver::solve: Array value type and solver value type are different." );
+            "Cabana::Grid::HypreStructuredSolver::solve: Array value type and "
+            "solver value type are different." );
 
         // This function is only valid for non-preconditioners.
         if ( _is_preconditioner )
-            throw std::logic_error( "Cabana::Grid::HypreStructuredSolver::solve: Cannot call solve() on preconditioners" );
+            throw std::logic_error(
+                "Cabana::Grid::HypreStructuredSolver::solve: Cannot call "
+                "solve() on preconditioners" );
 
         if ( b.layout()->dofsPerEntity() != 1 ||
              x.layout()->dofsPerEntity() != 1 )
             throw std::runtime_error(
-                "Cabana::Grid::HypreStructuredSolver::solve: Structured solver only for scalar fields" );
+                "Cabana::Grid::HypreStructuredSolver::solve: Structured solver "
+                "only for scalar fields" );
 
         // Spatial dimension.
         const std::size_t num_space_dim = Array_t::num_space_dim;
@@ -1515,7 +1531,8 @@ createHypreStructuredSolver( const std::string& solver_type,
         return createHypreStructDiagonal<Scalar, MemorySpace>(
             layout, is_preconditioner );
     else
-        throw std::runtime_error( "Cabana::Grid::createHypreStructuredSolver: Invalid solver type" );
+        throw std::runtime_error(
+            "Cabana::Grid::createHypreStructuredSolver: Invalid solver type" );
 }
 
 //---------------------------------------------------------------------------//
