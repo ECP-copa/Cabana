a08dc495dc52ac1a5de993c6e52b7f54afb75bed
Fabien Péan <pean@virtonomy.io>
2025-07-21 16:34:29 +0000

    Fix formatting

--- core/src/Cabana_VerletList.hpp
+++ core/src/Cabana_VerletList.hpp
@@ -857,16 +857,17 @@ class VerletList
       whether or not they are in the range.
     */
     template <class PositionSlice, class RadiusSlice>
-    VerletList( PositionSlice x, const std::size_t begin, const std::size_t end,
-                const typename PositionSlice::value_type background_radius,
-                RadiusSlice neighborhood_radius,
-                const typename PositionSlice::value_type cell_size_ratio,
-                const typename PositionSlice::value_type grid_min[3],
-                const typename PositionSlice::value_type grid_max[3],
-                const std::size_t max_neigh = 0,
-                typename std::enable_if<( is_slice<PositionSlice>::value ||
-                                          Kokkos::is_view<PositionSlice>::value ),
-                                        int>::type* = 0 )
+    VerletList(
+        PositionSlice x, const std::size_t begin, const std::size_t end,
+        const typename PositionSlice::value_type background_radius,
+        RadiusSlice neighborhood_radius,
+        const typename PositionSlice::value_type cell_size_ratio,
+        const typename PositionSlice::value_type grid_min[3],
+        const typename PositionSlice::value_type grid_max[3],
+        const std::size_t max_neigh = 0,
+        typename std::enable_if<( is_slice<PositionSlice>::value ||
+                                  Kokkos::is_view<PositionSlice>::value ),
+                                int>::type* = 0 )
     {
         build( x, begin, end, background_radius, neighborhood_radius,
                cell_size_ratio, grid_min, grid_max, max_neigh );
