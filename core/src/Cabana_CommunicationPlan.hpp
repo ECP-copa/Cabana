c757eb3f3673ccad20c4c3d06f9e334984ac34b3
Jason Stewart <jason00stewart@gmail.com>
2025-08-15 11:52:48 -0600

    Added an MPI_Barrier to stop race condition

--- core/src/Cabana_CommunicationPlan.hpp
+++ core/src/Cabana_CommunicationPlan.hpp
@@ -1063,6 +1063,12 @@ class CommunicationPlan
         if ( MPI_SUCCESS != ec )
             throw std::logic_error( "Failed MPI Communication" );
 
+        // This barrier is needed to ensure all the above Isends and IRecvs
+        // complete before the next echange starts. If there is no barrier
+        // sometimes the send_to data will be populated incorrectly and cause
+        // the code to hang.
+        MPI_Barrier( comm() );
+
         // Get the total number of imports/exports.
         _total_num_export =
             std::accumulate( _num_export.begin(), _num_export.end(), 0 );
@@ -1306,6 +1312,13 @@ class CommunicationPlan
             MPI_Waitall( num_recvs, mpi_requests.data(), mpi_statuses.data() );
         if ( MPI_SUCCESS != ec0 )
             throw std::logic_error( "Failed MPI Communication" );
+
+        // This barrier is needed to ensure all the above Isends and IRecvs
+        // complete before the next echange starts. If there is no barrier
+        // sometimes the send_to data will be populated incorrectly and cause
+        // the code to hang.
+        MPI_Barrier( comm() );
+
         // Save ranks we got messages from and track total messages to size
         // buffers
         _total_num_export = 0;
