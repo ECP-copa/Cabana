6c10418cbef2a798210e18fe88038d71ce219dcf
Jason Stewart <jason00stewart@gmail.com>
2025-08-25 10:54:43 -0600

    Update #include from Halo to HaloBase

--- grid/unit_test/tstIndexConversion.hpp
+++ grid/unit_test/tstIndexConversion.hpp
@@ -12,7 +12,7 @@
 #include <Cabana_Grid_Array.hpp>
 #include <Cabana_Grid_GlobalGrid.hpp>
 #include <Cabana_Grid_GlobalMesh.hpp>
-#include <Cabana_Grid_Halo.hpp>
+#include <Cabana_Grid_HaloBase.hpp>
 #include <Cabana_Grid_IndexConversion.hpp>
 #include <Cabana_Grid_IndexSpace.hpp>
 #include <Cabana_Grid_LocalGrid.hpp>
